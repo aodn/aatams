75ec1e21c68ac3373c5f4598cbdffdc290f7c43b
stevec <stevec@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2009-09-28 04:14:52 +0000

    initial import

---
+++ data file processing/src/au/org/emii/file/processing/AcousticReceiverFileProcessor.java
@@ -0,0 +1,297 @@
+/**
+ * 
+ */
+package au.org.emii.file.processing;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.apache.log4j.Level;
+
+
+//TODO add deployment_download_id to detection table to associate detections with a specific file.
+//this allows a final cross-check of detections in database after commit with those in file.
+/**
+ * @author Stephen Cameron
+ * 
+ */
+public class AcousticReceiverFileProcessor {
+	private static final String timestampFormat = "dd/MM/yyyy  hh:mm:ss ";
+	public static final SimpleDateFormat timestampParser = new SimpleDateFormat(timestampFormat);
+	private static final String pattern = "\"([^\"]+?)\",?|([^,]+),?|,";
+	private static final Pattern csvRegex = Pattern.compile(pattern);
+	private static final String DOWNLOAD_ID_PREFIX = "aatams.dployment_download.";
+	private Logger logger = Logger.getLogger(this.getClass());
+	public void doProcessing(
+			String downloadId,
+			String filePath,
+			String jdbcDriverClassName,
+			String connectionString,
+			String username,
+			String password){
+		Connection conn = null;
+		try {
+			// Load the database driver
+			Class.forName(jdbcDriverClassName);
+			// Get a connection to the database
+			conn = DriverManager.getConnection(connectionString, username, password);
+			// Print all warnings
+			for (SQLWarning warn = conn.getWarnings(); warn != null; warn = warn.getNextWarning()) {
+				logger.warn("SQL Warning:");
+				logger.warn("State  : " + warn.getSQLState());
+				logger.warn("Message: " + warn.getMessage());
+				logger.warn("Error  : " + warn.getErrorCode());
+			}
+			//Load the file
+			File file = new File(filePath);
+			if(!file.exists()){
+				throw new Exception("invalid file path: " + filePath);
+			}
+			if(!file.canRead()){
+				throw new Exception("file at "+  filePath + " cannot be opened for reading");
+			}
+			//1. Parse the file and get a unique list of tags - there are usually lots of duplicates
+			//Also check that there is only one receiver in the file.
+			HashMap<String,Tag> tags = new HashMap<String,Tag>();
+			ArrayList<Detection> detections = new ArrayList<Detection>();
+			String receiverName = null;
+			try {
+				// open file for reading
+				logger.info("reading file: " + file.getAbsolutePath());
+				FileInputStream fis = new FileInputStream(file);
+				BufferedReader  buffer = new BufferedReader(new InputStreamReader(fis, "UTF-8"));
+				String line = buffer.readLine();
+				if(this.validHeader(line)){
+					while((line = buffer.readLine())!= null){
+						List<String> values = this.parseLine(line);
+						//id is made of code-space and ID
+						String tagName = values.get(1) + "-" + values.get(2);
+						if(tags.containsKey(tagName)){
+							tags.get(tagName).detectionCount++;
+						}else{
+							tags.put(tagName,new Tag(tagName));
+						}
+						if(receiverName != null && values.get(9).equals(receiverName)){
+							throw new Exception("more than one receiver is in the file");
+						}else{
+							receiverName = values.get(9);
+						}
+						//buffer the record
+						try{
+							detections.add(new Detection(tags.get(tagName), 
+									new Timestamp(timestampParser.parse(values.get(0)).getTime())));
+						}catch(ParseException e){
+							throw new Exception("an invalid timestamp string has been found ("+values.get(0)+"), expecting format ");
+						}
+					}
+				}
+				logger.info("detection count: " + detections.size());
+				fis.close();
+			}catch(Exception e){
+				
+			}
+			//2. Find the database ids of the tags detected.
+			PreparedStatement pst = conn.prepareStatement(
+		    "SELECT DEVICE_ID FROM DEVICE WHERE CODE_NAME = ? AND DEVICE_TYPE_ID = 2");
+			ResultSet rset;
+			for(Iterator<Entry<String,Tag>> i = tags.entrySet().iterator(); i.hasNext();){
+				Tag tag = i.next().getValue();
+				pst.setString(1,tag.name);
+				pst.execute();
+				rset = pst.getResultSet();
+				if(rset != null){
+					if(rset.first()){
+						tag.id = rset.getInt(0);
+					}
+				}
+				rset.close();
+			}
+			//3. Check that the deployment_download record exists and is linked to the correct receiver
+			Integer projectRolePersonId = 0;
+			Integer receiverDeploymentId = 0;
+			if(downloadId != null){
+				if(downloadId.startsWith(DOWNLOAD_ID_PREFIX)){
+					Integer id;
+					try{
+						id = Integer.valueOf(downloadId.substring(DOWNLOAD_ID_PREFIX.length()));
+					}catch(NumberFormatException e){
+						throw new Exception("the downloadId parameter passed is invalid, it must end with a number", e);
+					}
+					Statement smt = conn.createStatement();
+					//a receiver deployment must have a project_role_person associated with it,
+					//extract this to be used for any new tags added.  
+					rset = smt.executeQuery("SELECT RECEIVER_DEPLOYMENT.DEPLOYMENT_ID, RECEIVER_DEPLOYMENT.PROJECT_ROLE_PERSON_ID FROM " +
+							"DEPLOYMENT_DOWNLOAD, RECEIVER_DEPLOYMENT, DEVICE WHERE " +
+							"DEPLOYMENT_DOWNLOAD.DEPLOYMENT_ID = RECEIVER_DEPLOYMENT.DEPLOYMENT_ID AND " +
+							"RECEIVER_DEPLOYMENT.DEVICE_ID = DEVICE.DEVICE_ID AND " +
+							"DEVICE.CODE_NAME = '" + receiverName + "'" +
+							"DOWNLOAD_ID = "+ id);
+					if(rset != null){ 
+						if(rset.first()){
+							receiverDeploymentId = rset.getInt(0);
+							projectRolePersonId = rset.getInt(1);
+							if(projectRolePersonId == 0){
+								logger.info("could find a project_role_person record id to use for adding unknown tags to database"); 
+							}
+						}else{
+							throw new Exception("a matching 'deployment download' record has not been found in the database for '"+ downloadId + "'" +
+									" being linked to a receiver_deployment with receiver device code named '" + receiverName + "'"); 
+						}
+					}
+					rset.close();
+				}else{
+					throw new Exception("the downloadId parameter passed is invalid, it must start with '" + DOWNLOAD_ID_PREFIX +  "'");
+				}
+			}else{
+				throw new Exception("the downloadId parameter passed is null");
+			}
+			//4. Find or create new tag records in the database.
+			HashMap<String,Tag> newTags = new HashMap<String,Tag>();
+			pst = conn.prepareStatement("INSERT INTO DEVICE (DEVICE_ID,DEVICE_TYPE_ID,CODE_NAME,PROJECT_ROLE_PERSON_ID)" +
+					" VALUES (DEVICE_SERIAL.NEXTVAL(),2,?,?)");
+			for(Iterator<Entry<String,Tag>> i = tags.entrySet().iterator(); i.hasNext();){
+				Tag tag = i.next().getValue();
+				if(tag.id == 0){
+					pst.setString(1,tag.name);
+					pst.setInt(2,projectRolePersonId);
+					pst.execute();
+					rset = pst.getGeneratedKeys();
+					if(rset != null){ 
+						if(rset.first()){
+							tag.id = rset.getInt(0);
+						}
+					}
+					rset.close();
+					newTags.put(tag.name,tag);
+				}
+			}
+			//5. Create new detection records in the database.
+			pst = conn.prepareStatement("INSERT INTO DETECTION (DETECTION_ID, DEPLOYMENT_ID, TAG_ID, DETECTION_TIMESTAMP)" +
+			" VALUES (DETECTION_SERIAL.NEXTVAL(),"+ receiverDeploymentId +",?,?)");
+			for(Iterator<Detection> i = detections.iterator(); i.hasNext();){
+				Detection detection = i.next();
+				pst.setInt(0, detection.tag.id);
+				pst.setTimestamp(1, detection.timestamp);
+				pst.execute();
+			}
+			//happy ending
+			conn.commit();
+			conn.close();
+			//6. Prepare a report(log) file to be sent back to the submitter and data manager.
+			logger.info("deployment download id = " + downloadId);
+			logger.info("receiver deployment id = " + receiverDeploymentId);
+			logger.info("project role person id = " + projectRolePersonId);
+			logger.info("new tags added:");
+			for(Iterator<Entry<String,Tag>> i = tags.entrySet().iterator(); i.hasNext();){
+				Tag tag = i.next().getValue();
+				logger.info("tag code_name = " + tag.name + ", id = " + tag.id);
+			}
+			logger.info("total new detections added = " + detections.size());
+		} catch (SQLException se) {
+			logger.fatal("SQL Exception:");
+			// Loop through the SQL Exceptions
+			while (se != null) {
+				logger.fatal("State  : " + se.getSQLState());
+				logger.fatal("Message: " + se.getMessage());
+				logger.fatal("Error  : " + se.getErrorCode());
+				se = se.getNextException();
+			}
+			try{
+				if(conn != null && !conn.isClosed()){
+					conn.rollback();
+				}
+			}catch(SQLException e){
+				logger.warn("attempted rollback produced an SQLException", e);
+				return;
+			}
+		} catch (Exception e) {
+			logger.fatal(e);
+			return;
+		}
+	}
+	
+	
+	private boolean validHeader(String line){
+		return true;
+	}
+	
+	/**
+	 * Parse one line.
+	 * 
+	 * @return List of Strings, minus their double quotes
+	 */
+	private List<String> parseLine(String line) {
+		
+		List<String> list = new ArrayList<String>();
+		Matcher m = csvRegex.matcher(line);
+		// For each field
+		while (m.find()) {
+			String match = m.group();
+			if (match == null)
+				break;
+			if (match.endsWith(",")) { // trim trailing ,
+				match = match.substring(0, match.length() - 1);
+			}
+			if (match.startsWith("\"")) { // assume also ends with
+				match = match.substring(1, match.length() - 1);
+			}
+			if (match.length() == 0)
+				match = null;
+			list.add(match);
+		}
+		return list;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		
+		AcousticReceiverFileProcessor processor = new AcousticReceiverFileProcessor();
+		if(args.length == 6){
+			processor.doProcessing(args[0],args[1],args[2],args[3],args[4], args[5]);
+		}else{
+			processor.logger.error("6 parameters are needed: download id, file path, jdbc driver class, connection string, username and password");
+		}
+
+	}
+	
+	private class Detection{
+		
+		public Timestamp timestamp;	
+		public Tag tag;
+		
+		public Detection(Tag tag, Timestamp detectionDatetime){
+			assert(tag != null);
+			assert(detectionDatetime != null);
+			this.tag = tag;
+			this.timestamp = detectionDatetime;
+		}
+	}
+	
+	private class Tag{
+		
+		public String name;
+		public int id = 0;
+		public Integer detectionCount = 1;
+		
+		public Tag(String tagName){
+			assert(tagName != null);
+			this.name = tagName;
+		}
+	}
+}

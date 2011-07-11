9c0984b09adce8dcc3d23093481b50a518c4390a
jburgess <jburgess@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-07-11 04:56:56 +0000

    1. Homepage now lists projects.

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,17 @@
-class UrlMappings {
+class UrlMappings 
+{
 
-	static mappings = {
-		"/$controller/$action?/$id?"{
-			constraints {
+	static mappings = 
+        {
+		"/$controller/$action?/$id?"
+                {
+			constraints 
+                        {
 				// apply constraints here
 			}
 		}
 
-		"/"(view:"/index")
+		"/"(controller:"project")
 		"500"(view:'/error')
 	}
 }

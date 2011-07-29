573dd107c4a8c1845372a1589c26419f8183644c
Jon Burgess <jkburges@gmail.com>
2011-07-29 01:15:59 +0000

    1. Fix up unit tests so that they all pass.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -41,11 +41,14 @@ environments {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop','update'
             driverClassName = "org.postgresql.Driver"
-            url = "jdbc:postgresql://localhost:5432/aatams"
+            url = "jdbc:postgresql://localhost:5433/aatams_test"
             username = "aatams"
             password = "aatams"
-            
         }
+//        dataSource {
+//            dbCreate = "update"
+//            url = "jdbc:hsqldb:mem:testDb"
+//        }
     }
     
     production 
--- test/unit/au/org/emii/aatams/PersonControllerTests.groovy
+++ test/unit/au/org/emii/aatams/PersonControllerTests.groovy
@@ -2,16 +2,56 @@ package au.org.emii.aatams
 
 import grails.test.*
 
-class PersonControllerTests extends ControllerUnitTestCase {
-    protected void setUp() {
+import org.apache.shiro.subject.Subject
+import org.apache.shiro.util.ThreadContext
+import org.apache.shiro.SecurityUtils
+
+class PersonControllerTests extends ControllerUnitTestCase 
+{
+    protected void setUp() 
+    {
         super.setUp()
+        TestUtils.setupMessage(controller)
     }
 
-    protected void tearDown() {
+    protected void tearDown() 
+    {
         super.tearDown()
     }
 
-    void testSomething() {
-
+    void testShow() 
+    {
+        TestUtils.loginSysAdmin()
+        
+        def address = new Address(id:1,
+                                  streetAddress:'1 Smith Street',
+                                  suburbTown:'Moonah',
+                                  state:'TAS',
+                                  postcode:'7000',
+                                  country:'Australia')
+        
+        // Create organisation.
+        def imos = new Organisation(id:2,
+                                    name:'IMOS',
+                                    department:'eMII',
+                                    phoneNumber:'12345678',
+                                    faxNumber:'12345678',
+                                    streetAddress:address,
+                                    postalAddress:address,
+                                    status:EntityStatus.ACTIVE)
+        
+        def joeBloggs = new Person(id:3,
+                                   organisation:imos,
+                                   name:'Joe Bloggs',
+                                   emailAddress:'jbloggs@imos.org.au',
+                                   phoneNumber:'12345678',
+                                   status:EntityStatus.ACTIVE)
+        mockDomain(Person, [joeBloggs])                      
+        
+        controller.params.id = 3
+        def returnValue = controller.show()
+        println(returnValue)
+        
+        assertSame(returnValue.personInstance, joeBloggs)
     }
 }

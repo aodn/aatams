c1d7b6600182af45542b0d942b5edf60c14e26ae
Jon Burgess <jkburges@gmail.com>
2011-08-03 06:18:40 +0000

    1. Removed unnecessary log.

--- web-app/js/speciesLookup.js
+++ web-app/js/speciesLookup.js
@@ -1,9 +1,6 @@
 $(function()
 {
-//    ${createLink(controller:"species", action:"lookupByName")}
     var dataSource = "/aatams/species/lookupByName"
-    console.log("dataSource: " + dataSource)
-    
     $("#speciesName").autocomplete({source:dataSource});
 });
         

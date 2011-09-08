7614528ac1f0f115f305c7f5665be36199adfdb4
jburgess <jburgess@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-09-08 01:44:09 +0000

    1. Tests for SecUser.

---
+++ test/unit/SecUserTests.groovy
@@ -0,0 +1,61 @@
+import grails.test.*
+
+class SecUserTests extends GrailsUnitTestCase 
+{
+    protected void setUp() 
+    {
+        super.setUp()
+        
+        mockDomain(SecUser)
+    }
+
+    protected void tearDown()
+    {
+        super.tearDown()
+    }
+
+    void testNullUsername() 
+    {
+        SecUser user = new SecUser(passwordHash:"asdf")
+        mockForConstraintsTests(SecUser, [user])
+        
+        assertFalse(user.validate())
+    }
+    
+    void testBlankUsername()
+    {
+        SecUser user = new SecUser(username:"", passwordHash:"asdf")
+        mockForConstraintsTests(SecUser, [user])
+        
+        assertFalse(user.validate())
+    }
+
+    void testNonUniqueUsername()
+    {
+        SecUser user1 = new SecUser(username:"aaa", passwordHash:"asdf")
+        mockForConstraintsTests(SecUser, [user1])
+        
+        assertTrue(user1.validate())
+
+        SecUser user2 = new SecUser(username:"aaa", passwordHash:"asdf")
+        mockForConstraintsTests(SecUser, [user1, user2])
+        
+        assertFalse(user2.validate())
+    }
+ 
+    void testNullPassword()
+    {
+        SecUser user = new SecUser(username:"")
+        mockForConstraintsTests(SecUser, [user])
+        
+        assertFalse(user.validate())
+    }
+    
+    void testBlankPassword()
+    {
+        SecUser user = new SecUser(username:"", passwordHash:"")
+        mockForConstraintsTests(SecUser, [user])
+        
+        assertFalse(user.validate())
+    }
+}

adcc30ed347ad93ace2d5762ad2aec102cf3f4d7
jburgess <jburgess@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-09-12 00:03:27 +0000

    1. Implemented cache clearing for DB realm.

--- grails-app/realms/CachedDbRealm.groovy
+++ grails-app/realms/CachedDbRealm.groovy
@@ -7,10 +7,34 @@ class CachedDbRealm extends SecDbRealm
     // This is here for unit testing
     boolean cacheHit = false
     
+    /**
+     * Keyed by principal, with each principal's map in turn being a map
+     * of permission name to bool value.
+     */
     private Map<Object, Map<String, Boolean>> cache = [:]
+    
+    /**
+     * Becomes true when something happens that means the cache is out of date
+     */
+    static boolean cacheIsDirty = false
+
+    static markCacheDirty()
+    {
+        cacheIsDirty = true
+    }
+    
+    def checkCache()
+    {
+        if (cacheIsDirty)
+        {
+            cache = [:]
+            cacheIsDirty = false
+        }
+    }
     
     def isPermitted(principal, requiredPermission) 
     {
+        checkCache()
         cacheHit = false
        
         // Is the principal in the cache?
@@ -37,6 +61,7 @@ class CachedDbRealm extends SecDbRealm
         assert(retVal != null): "retVal cannot be null"
         
         return retVal
-    }	
+    }
+    
 }
 
--- test/unit/CachedDbRealmTests.groovy
+++ test/unit/CachedDbRealmTests.groovy
@@ -1,8 +1,20 @@
 import grails.test.*
 
+import au.org.emii.aatams.*
+
+import org.apache.shiro.subject.Subject
+import org.apache.shiro.util.ThreadContext
+import org.apache.shiro.SecurityUtils
+
 class CachedDbRealmTests extends GrailsUnitTestCase 
 {
     def realm
+    
+    PermissionUtilsService service
+    ProjectRoleType piRoleType
+    Person user
+    Project project
+    ProjectRoleType nonPiType
     
     protected void setUp() 
     {
@@ -10,9 +22,10 @@ class CachedDbRealmTests extends GrailsUnitTestCase
 
         realm = new CachedDbRealm()
         
-        SecUser user = new SecUser(username:"user", passwordHash:"asdf")
-        SecUser userDiff = new SecUser(username:"userDiff", passwordHash:"asdf")
+        user = new Person(username:"user", passwordHash:"asdf", name:Person)
+        Person userDiff = new Person(username:"userDiff", passwordHash:"asdf")
         def userList = [user, userDiff]
+        mockDomain(Person, userList)
         mockDomain(SecUser, userList)
         userList.each{ it.save() }
         
@@ -21,6 +34,30 @@ class CachedDbRealmTests extends GrailsUnitTestCase
             query ->
             return null
         }
+        
+        mockLogging(PermissionUtilsService, true)
+        service = new PermissionUtilsService()
+        piRoleType = new ProjectRoleType(displayName:ProjectRoleType.PRINCIPAL_INVESTIGATOR)
+        nonPiType = new ProjectRoleType(displayName:"Non PI")
+        def roleTypeList = [piRoleType, nonPiType]
+        mockDomain(ProjectRoleType, roleTypeList)
+        roleTypeList.each{it.save()}
+        
+        mockDomain(ProjectRole)
+        
+        project = new Project(name:"project")
+        mockDomain(Project, [project])
+        project.save()
+        
+        def subject = [ getPrincipal: { user.username },
+                        isAuthenticated: { true },
+                        hasRole: { true } 
+                      ] as Subject
+
+        ThreadContext.put( ThreadContext.SECURITY_MANAGER_KEY, 
+                            [ getSubject: { subject } ] as SecurityManager )
+
+        SecurityUtils.metaClass.static.getSubject = { subject }
     }
 
     protected void tearDown() 
@@ -121,5 +158,102 @@ class CachedDbRealmTests extends GrailsUnitTestCase
         // Same user and permission (hit)
         assertFalse(realm.isPermitted("user", "write"))
         assertTrue(realm.getCacheHit())
+    }
+    
+    void testSetPermissionClearsCache()
+    {
+        String permission = "project:" + project.id + ":read"
+
+        // First time will be a miss
+        realm.isPermitted(user.username, permission)
+        assertFalse(realm.getCacheHit())
+
+        // Second time will be a hit
+        realm.isPermitted(user.username, permission)
+        assertTrue(realm.getCacheHit())
+        
+        // Now we make a call to permission util service, which should result
+        // in the cache being dirtied.
+        ProjectRole nonPiRead = 
+            new ProjectRole(project:project,
+                            person:user,
+                            roleType:nonPiType,
+                            access:ProjectAccess.READ_ONLY)
+                        
+        service.setPermissions(nonPiRead)
+        user.permissions = null // Do this so that SecDbRealm doesn't throw NPE
+
+        realm.isPermitted(user.username, permission)
+        assertFalse(realm.getCacheHit())
+    }
+    
+    void testRemovePermissionClearsCache()
+    {
+        String permission = "project:" + project.id + ":read"
+
+        // First time will be a miss
+        realm.isPermitted(user.username, permission)
+        assertFalse(realm.getCacheHit())
+
+        // Second time will be a hit
+        realm.isPermitted(user.username, permission)
+        assertTrue(realm.getCacheHit())
+        
+        // Now we make a call to permission util service, which should result
+        // in the cache being dirtied.
+        ProjectRole nonPiRead = 
+            new ProjectRole(project:project,
+                            person:user,
+                            roleType:nonPiType,
+                            access:ProjectAccess.READ_ONLY)
+                        
+        service.removePermissions(nonPiRead)
+        
+        realm.isPermitted(user.username, permission)
+        assertFalse(realm.getCacheHit())
+    }
+    
+    void testReceiverCreatedClearsCache()
+    {
+        Receiver rx = new Receiver(id:12)
+        String permission = service.buildReceiverUpdatePermission(rx.id)
+        
+        // First time will be a miss
+        realm.isPermitted(user.username, permission)
+        assertFalse(realm.getCacheHit())
+
+        // Second time will be a hit
+        realm.isPermitted(user.username, permission)
+        assertTrue(realm.getCacheHit())
+        
+        // Now we make a call to permission util service, which should result
+        // in the cache being dirtied.
+        service.receiverCreated(rx)
+        user.permissions = null // Do this so that SecDbRealm doesn't throw NPE
+
+        realm.isPermitted(user.username, permission)
+        assertFalse(realm.getCacheHit())
+    }
+    
+    void testReceiverDeletedClearsCache()
+    {
+        Receiver rx = new Receiver(id:12)
+        String permission = service.buildReceiverUpdatePermission(rx.id)
+        
+        // First time will be a miss
+        realm.isPermitted(user.username, permission)
+        assertFalse(realm.getCacheHit())
+
+        // Second time will be a hit
+        realm.isPermitted(user.username, permission)
+        assertTrue(realm.getCacheHit())
+        
+        // Now we make a call to permission util service, which should result
+        // in the cache being dirtied.
+        service.receiverCreated(rx)
+        user.permissions = null // Do this so that SecDbRealm doesn't throw NPE
+
+        realm.isPermitted(user.username, permission)
+        assertFalse(realm.getCacheHit())
     }
 }

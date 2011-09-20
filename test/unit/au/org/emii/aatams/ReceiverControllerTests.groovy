ffc76fedb9712204de3a44f15f2130754614f23e
Jon Burgess <jkburges@gmail.com>
2011-09-20 07:24:00 +0000

    1. Fix for #571 - automate receiver status where possible.

--- test/unit/au/org/emii/aatams/ReceiverControllerTests.groovy
+++ test/unit/au/org/emii/aatams/ReceiverControllerTests.groovy
@@ -2,16 +2,65 @@ package au.org.emii.aatams
 
 import grails.test.*
 
-class ReceiverControllerTests extends ControllerUnitTestCase {
-    protected void setUp() {
+class ReceiverControllerTests extends ControllerUnitTestCase 
+{
+
+    DeviceStatus newStatus
+    DeviceStatus deployedStatus
+    DeviceStatus recoveredStatus
+
+    def imos
+    
+    def permissionUtilsService
+    
+    protected void setUp() 
+    {
         super.setUp()
+        
+        newStatus = new DeviceStatus(status:"NEW")
+        deployedStatus = new DeviceStatus(status:"DEPLOYED")
+        recoveredStatus = new DeviceStatus(status:"RECOVERED")
+        
+        def statusList = [newStatus, deployedStatus, recoveredStatus]
+        mockDomain(DeviceStatus, statusList)
+        statusList.each { it.save() }
+        
+        imos = new Organisation(name:"IMSO")
+        def csiro = new Organisation(name:"CSIRO")
+        def orgList = [imos, csiro]
+        mockDomain(Organisation, orgList)
+        orgList.each { it.save() }
+        
+        mockDomain(Receiver)
+        
+        mockLogging(PermissionUtilsService)
+        permissionUtilsService = new PermissionUtilsService()
+        permissionUtilsService.metaClass.receiverCreated =
+        {
+            // no-op.
+        }
+        
+        controller.permissionUtilsService = permissionUtilsService
+        controller.metaClass.message = {}
     }
 
-    protected void tearDown() {
+    protected void tearDown() 
+    {
         super.tearDown()
     }
 
-    void testSomething() {
-
+    void testReceiverSave() 
+    {
+        controller.params.organisation = imos
+        controller.params.model = new DeviceModel(modelName:"model")
+        controller.params.serialNumber = "12345"
+        controller.params.comment = ""
+        
+        controller.save()
+        def receiver = Receiver.get(controller.redirectArgs.id)
+        
+        assertEquals("show", controller.redirectArgs.action)
+        assertEquals("model-12345", receiver.codeName)
+        assertEquals(newStatus, receiver.status)
     }
 }

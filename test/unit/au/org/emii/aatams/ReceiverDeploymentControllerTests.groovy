e7102d4b1e5ae9d4bc1935e18771958f58c7c847
jburgess <jburgess@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-09-20 05:26:38 +0000

    1. Fix for #573 - can no longer deploy a deployed receiver.

--- test/unit/au/org/emii/aatams/ReceiverDeploymentControllerTests.groovy
+++ test/unit/au/org/emii/aatams/ReceiverDeploymentControllerTests.groovy
@@ -2,16 +2,91 @@ package au.org.emii.aatams
 
 import grails.test.*
 
-class ReceiverDeploymentControllerTests extends ControllerUnitTestCase {
-    protected void setUp() {
+import org.joda.time.*
+
+class ReceiverDeploymentControllerTests extends ControllerUnitTestCase 
+{
+    Receiver newReceiver
+    Receiver deployedReceiver
+    Receiver recoveredReceiver
+    Receiver csiroReceiver
+
+    DeviceStatus newStatus
+    DeviceStatus deployedStatus
+    DeviceStatus recoveredStatus
+
+    protected void setUp() 
+    {
         super.setUp()
+
+        newStatus = new DeviceStatus(status:"NEW")
+        deployedStatus = new DeviceStatus(status:"DEPLOYED")
+        recoveredStatus = new DeviceStatus(status:"RECOVERED")
+        
+        def statusList = [newStatus, deployedStatus, recoveredStatus]
+        mockDomain(DeviceStatus, statusList)
+        statusList.each { it.save() }
+        
+        def imos = new Organisation(name:"IMSO")
+        def csiro = new Organisation(name:"CSIRO")
+        def orgList = [imos, csiro]
+        mockDomain(Organisation, orgList)
+        orgList.each { it.save() }
+
+        newReceiver = new Receiver(codeName:"VRW2-111", status:newStatus, organisation:imos)
+        deployedReceiver = new Receiver(codeName:"VRW2-222", status:deployedStatus, organisation:imos)
+        recoveredReceiver = new Receiver(codeName:"VRW2-333", status:recoveredStatus, organisation:imos)
+        csiroReceiver = new Receiver(codeName:"VRW2-444", status:recoveredStatus, organisation:csiro)
+
+        def receiverList = [newReceiver, deployedReceiver, recoveredReceiver]
+        mockDomain(Receiver, receiverList)
+        receiverList.each 
+        {
+            imos.addToReceivers(it)
+            it.save() 
+        }
+        
+        imos.save()
+        
+        mockDomain(ReceiverDeployment)
+        
+        controller.metaClass.message = { LinkedHashMap args -> return args }
     }
 
-    protected void tearDown() {
+    protected void tearDown() 
+    {
         super.tearDown()
     }
 
-    void testSomething() {
+    void testSaveNewReceiver() 
+    {
+        controller.params.receiver = newReceiver
+        controller.params.deploymentDateTime = new DateTime()
+        controller.params.mooringType = new MooringType()
+        controller.params.station = new InstallationStation()
+        
+        controller.save()
+        
+        assertEquals("show", controller.redirectArgs.action)
+        assertEquals(deployedStatus, newReceiver.status)
+    }
 
+    void testSaveDeployedReceiver() 
+    {
+        controller.params.receiver = deployedReceiver
+        controller.params.deploymentDateTime = new DateTime()
+        controller.params.mooringType = new MooringType()
+        controller.params.station = new InstallationStation()
+        
+        controller.save()
+        
+        assertEquals("create", controller.renderArgs.view)
+        assertNotNull(controller.renderArgs.model.receiverDeploymentInstance)
+        assertNull(controller.renderArgs.model.receiverDeploymentInstance.receiver)
+        
+        def message = "${[code: 'default.invalidState.receiver', \
+                       args: [deployedReceiver.toString(), \
+                              deployedStatus.toString()]]}"
+        assertEquals(message, controller.flash.message)
     }
 }

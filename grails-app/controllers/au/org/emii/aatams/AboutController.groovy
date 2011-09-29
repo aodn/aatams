7d4f160b34a27a393774f7b6e4564431dcb27cf9
jburgess <jburgess@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-09-29 05:03:19 +0000

    1. Fix for #541, #556 - home page set depending on authenticated or not.

--- grails-app/controllers/au/org/emii/aatams/AboutController.groovy
+++ grails-app/controllers/au/org/emii/aatams/AboutController.groovy
@@ -1,8 +1,22 @@
 package au.org.emii.aatams
 
+import org.apache.shiro.SecurityUtils
+
 class AboutController 
 {
-    def index = 
+    def home = 
+    {
+        if (SecurityUtils.subject.isAuthenticated())
+        {
+            redirect(controller:"gettingStarted")
+        }
+        else
+        {
+            render(view:"index")
+        }
+    }
+    
+    def index =
     {
         render(view:"index")
     }

1ae8b9ef80c5c3e11037e57a42a12b45433fbb46
Jon Burgess <jkburges@gmail.com>
2011-10-13 01:22:18 +0000

    1. Fix test datasource.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -24,20 +24,20 @@ environments {
     }
 
     test {
-//        dataSource {
-//            dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-//            driverClassName = "org.postgresql.Driver"
-//            url = "jdbc:postgresql://dbtest.emii.org.au:5432/aatams_test?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
-//            username = "aatams"
-//            password = "fishybusiness"
-//        }
         dataSource {
-            dbCreate = "create" // one of 'create', 'create-drop','update'
+            dbCreate = "create-drop" // one of 'create', 'create-drop','update'
             driverClassName = "org.postgresql.Driver"
-            url = "jdbc:postgresql://localhost:5432/aatams"
+            url = "jdbc:postgresql://dbtest.emii.org.au:5432/aatams_test?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
             username = "aatams"
             password = "fishybusiness"
         }
+//        dataSource {
+//            dbCreate = "create" // one of 'create', 'create-drop','update'
+//            driverClassName = "org.postgresql.Driver"
+//            url = "jdbc:postgresql://localhost:5432/aatams"
+//            username = "aatams"
+//            password = "fishybusiness"
+//        }
     }
     
     production 

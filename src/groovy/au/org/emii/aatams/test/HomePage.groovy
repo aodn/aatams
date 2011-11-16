7366a071f3533db4c40cdf764c45746d81101168
jburgess <jburgess@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-11-16 22:04:07 +0000

    1. Added test for register notification.

--- src/groovy/au/org/emii/aatams/test/HomePage.groovy
+++ src/groovy/au/org/emii/aatams/test/HomePage.groovy
@@ -6,6 +6,8 @@ import grails.plugins.selenium.pageobjects.UnexpectedPageException
 class HomePage extends Page 
 {
 	private static final String URL = "/aatams"
+
+	static final String REGISTER_ELEMENT_LOCATOR = "css=div.qtip-content.qtip-content"
 	
 	static HomePage open()
 	{
@@ -17,7 +19,6 @@ class HomePage extends Page
 	{
 		super(uri)
 	}
-
 
 	@Override
 	protected void verifyPage() throws UnexpectedPageException 
@@ -35,16 +36,46 @@ class HomePage extends Page
 		return new BannerPage()
 	}	
 	
-	public List<String> getNotifications()
+	public void refresh()
+	{
+		selenium.refresh()
+	}
+	
+	public List<String> getNotificationMessages()
 	{
 		List<String> notifications = new ArrayList()
-		notifications.add(selenium.getText("css=div.qtip-content.qtip-content"))
+		
+		if (selenium.isElementPresent(REGISTER_ELEMENT_LOCATOR))
+		{
+			notifications.add(selenium.getText(REGISTER_ELEMENT_LOCATOR))
+		}
 		
 		return notifications
 	}
+
+	public boolean isRegisterNotificationPresent()
+	{
+		return selenium.isElementPresent(REGISTER_ELEMENT_LOCATOR)
+	}	
 	
 	public String getLoginText()
 	{
 		return selenium.getText("link=Login")
+	}
+	
+	public void acknowledgeRegisterNotification()
+	{
+		selenium.click(REGISTER_ELEMENT_LOCATOR)
+	}
+	
+	public void waitForRegisterNotification()
+	{
+		selenium.waitForElementPresent(REGISTER_ELEMENT_LOCATOR)
+	}
+	
+	public void undoAcknowledgeRegisterNotification()
+	{
+		selenium.deleteAllVisibleCookies()
+//		selenium.deleteCookie("REGISTER", "path=/, domain=localhost")
 	}
 }
--- test/selenium/au/org/emii/aatams/HomePageTests.groovy
+++ test/selenium/au/org/emii/aatams/HomePageTests.groovy
@@ -10,23 +10,35 @@ import grails.plugins.selenium.SeleniumAware
 @Mixin(SeleniumAware)
 class HomePageTests extends GroovyTestCase
 {
+	HomePage homePage
+	
+	void setUp() 
+	{
+		homePage = HomePage.open()
+	}
+	
 	void testOpenHomePage() throws Exception
 	{
-		HomePage homePage = HomePage.open()
-		
-		assertTrue(homePage.getNotifications().contains("Click here to register to use AATAMS"))
-		
-		// TODO: acknowledge notification
-		// test it's not there
-		
 		assertEquals("Australian Animal Tagging and Monitoring System (AATAMS)", homePage.getAboutPage().getBodyHeading())
 		assertEquals("Login", homePage.getBannerPage().getLoginText())
+	}
+
+	void testAcknowledgeRegisterNotification()
+	{
+		homePage.undoAcknowledgeRegisterNotification()
+		homePage.refresh()
+		homePage.waitForRegisterNotification()
+		assertTrue(homePage.isRegisterNotificationPresent())
+		assertTrue(homePage.getNotificationMessages().contains("Click here to register to use AATAMS"))
+
+		homePage.acknowledgeRegisterNotification()
+		homePage.refresh()
+		assertFalse(homePage.isRegisterNotificationPresent())
+		assertFalse(homePage.getNotificationMessages().contains("Click here to register to use AATAMS"))
 	}
 	
 	void testLoginAsSysadmin()
 	{
-		HomePage homePage = HomePage.open()
-		
 		LoginPage loginPage = homePage.getBannerPage().login()
 		loginPage.typeUsername("jkburges")
 		loginPage.typePassword("password")
@@ -34,6 +46,6 @@ class HomePageTests extends GroovyTestCase
 		GettingStartedPage gettingStartedPage = (GettingStartedPage)loginPage.clickSignIn()
 		assertEquals("Getting Started", gettingStartedPage.getBodyHeading())
 		
-		homePage.getBannerPage().logout()
+		homePage = homePage.getBannerPage().logout()
 	}
 }

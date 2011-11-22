764c30e5b1b0f0c11fb8875de14e7fd71148a005
jburgess <jburgess@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-11-22 06:05:30 +0000

    1. Started implementation for "add person" test.

--- test/functional/pages/ProjectEditPage.groovy
+++ test/functional/pages/ProjectEditPage.groovy
@@ -1,5 +1,7 @@
 package pages
 
+import module.AddPersonToProjectDialog
+
 class ProjectEditPage extends ProjectCreateEditPage 
 {
 	static url = "project/edit"
@@ -13,5 +15,11 @@ class ProjectEditPage extends ProjectCreateEditPage
 	{
 		updateButton (to: [ProjectShowPage]) { $("input", value:"Update") }
 		deleteButton (to: [ProjectListPage]) { $("input", value:"Delete") }
+		
+		addPersonLink { $("a", id:"add_person_to_project") }
+		
+//		addPersonDialog { module AddPersonToProjectDialog, $("div", id:"dialog-form-add-person") }
+		addPersonDialog { module AddPersonToProjectDialog, $("div", id:"dialog-form-add-person").parent() }
+//		addPersonDialog { module AddPersonToProjectDialog, $("div", "aria-labelledby":"ui-dialog-title-dialog-form-add-person") }
 	}
 }

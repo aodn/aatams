4187ff08327a371aef0c153ddb5f4410097aaeda
Jon Burgess <jkburges@gmail.com>
2011-11-23 04:42:47 +0000

    1. Added CRUD functional tests for installation.

---
+++ test/functional/pages/InstallationCreateEditPage.groovy
@@ -0,0 +1,12 @@
+package pages
+
+class InstallationCreateEditPage extends LayoutPage 
+{
+	static content =
+	{
+		nameTextField { $("input", name: "name") }
+		configurationSelect { $("select", name: "configuration.id") }
+		projectSelect { $("select", name: "project.id") }
+
+	}
+}
---
+++ test/functional/pages/InstallationShowPage.groovy
@@ -0,0 +1,24 @@
+package pages
+
+class InstallationShowPage extends ShowPage 
+{
+	static url = "installation/show"
+	
+	static at =
+	{
+		title == "Show Installation"
+	}
+	
+	static content =
+	{
+		editButton(to: InstallationEditPage) { $("input", value: "Edit") }
+		deleteButton(to: InstallationListPage) { $("input", value: "Delete") }
+
+		row { $("td.name", text: it).parent() }
+		value { row(it).find("td.value").text() }
+		
+		name { value("Name") }
+		configurationLink { row("Configuration").find("a") }
+		projectLink { row("Project").find("a") }
+	}
+}

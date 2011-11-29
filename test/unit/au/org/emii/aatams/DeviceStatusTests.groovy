ccc098bd88db078b546df5c38fba8b2d174516dc
Jon Burgess <jkburges@gmail.com>
2011-11-29 05:14:01 +0000

    1. Fix for #882 - not including NEW or DEPLOYED in receiver recovery status.

---
+++ test/unit/au/org/emii/aatams/DeviceStatusTests.groovy
@@ -0,0 +1,33 @@
+package au.org.emii.aatams
+
+import grails.test.*
+
+class DeviceStatusTests extends GrailsUnitTestCase 
+{
+    protected void setUp() 
+	{
+        super.setUp()
+		
+		DeviceStatus newStatus = new DeviceStatus(status:'NEW')
+		DeviceStatus deployedStatus = new DeviceStatus(status:'DEPLOYED')
+		DeviceStatus recoveredStatus = new DeviceStatus(status:'RECOVERED')
+		DeviceStatus lostStatus = new DeviceStatus(status:'LOST')
+		DeviceStatus stolenStatus = new DeviceStatus(status:'STOLEN')
+		
+		def statusList = [newStatus, deployedStatus, recoveredStatus, lostStatus, stolenStatus]
+		mockDomain(DeviceStatus, statusList)
+		statusList.each { it.save() }
+    }
+
+    protected void tearDown() 
+	{
+        super.tearDown()
+    }
+
+    void testRecoveryStatuses() 
+	{
+		assertTrue(DeviceStatus.listRecoveryStatuses()*.status.containsAll(['RECOVERED', 'LOST', 'STOLEN']))
+		assertFalse(DeviceStatus.listRecoveryStatuses()*.status.contains('NEW'))
+		assertFalse(DeviceStatus.listRecoveryStatuses()*.status.contains('DEPLOYED'))
+    }
+}

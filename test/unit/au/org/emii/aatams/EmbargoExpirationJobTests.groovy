07dcb87884d013a07171c59a6cf0bb59a1cc561b
Jon Burgess <jkburges@gmail.com>
2011-12-07 01:35:57 +0000

    1. #866 - correct the link sent out in embargo expiration emails.

--- test/unit/au/org/emii/aatams/EmbargoExpirationJobTests.groovy
+++ test/unit/au/org/emii/aatams/EmbargoExpirationJobTests.groovy
@@ -1,22 +1,67 @@
 package au.org.emii.aatams
 
 import grails.test.*
-
 import org.apache.log4j.*
 
 class EmbargoExpirationJobTests extends GrailsUnitTestCase 
 {
     protected def job
     
+	AnimalRelease nullRelease
+	AnimalRelease yesterdayRelease
+	AnimalRelease todayRelease
+	AnimalRelease tomorrowRelease
+
     protected void setUp() 
     {
         super.setUp()
         
-        mockConfig("animalRelease { embargoExpiration {warningPeriodMonths = 1}}")
         mockDomain(Person)
 
         mockLogging(EmbargoExpirationJob, true)
         job = new EmbargoExpirationJob()
+		
+		Calendar yesterday = Calendar.getInstance()
+		yesterday.add(Calendar.DAY_OF_YEAR, -1)
+		Calendar tomorrow = Calendar.getInstance()
+		tomorrow.add(Calendar.DAY_OF_YEAR, 1)
+
+		Tag tag = new Tag(codeMap:new CodeMap(codeMap:'A69-1303'), pingCode:1234)
+		mockDomain(Tag, [tag])
+		tag.save()
+		
+		nullRelease = new AnimalRelease()
+		yesterdayRelease = new AnimalRelease(embargoDate: yesterday.getTime())
+		todayRelease = new AnimalRelease(embargoDate: new Date())
+		tomorrowRelease = new AnimalRelease(embargoDate: tomorrow.getTime())
+		
+		def releases = [nullRelease, yesterdayRelease, todayRelease, tomorrowRelease]
+		mockDomain(AnimalRelease, releases)
+		
+		nullRelease.addToSurgeries(new Surgery(release:nullRelease, tag:tag))
+		yesterdayRelease.addToSurgeries(new Surgery(release:yesterdayRelease, tag:tag))
+		todayRelease.addToSurgeries(new Surgery(release:todayRelease, tag:tag))
+		tomorrowRelease.addToSurgeries(new Surgery(release:tomorrowRelease, tag:tag))
+		
+		releases.each{ it.save() }
+		
+		def releaseCriteria = [list: {Closure cls -> [yesterdayRelease, todayRelease, tomorrowRelease]}]
+		AnimalRelease.metaClass.static.createCriteria = {releaseCriteria}
+		
+        mockConfig('''animalRelease 
+						{
+							embargoExpiration 
+							{
+								warningPeriodMonths = 1
+							}
+						}
+						
+						grails 
+						{
+							serverURL = "http://localhost:8090/aatams"
+						}''')
+//		job.grailsApplication = { -> [config: org.codehaus.groovy.grails.commons.ConfigurationHolder.config]}
+		job.metaClass.getServerUrl = { -> "http://localhost:8090/aatams" }
     }
 
     protected void tearDown() 
@@ -26,34 +71,23 @@ class EmbargoExpirationJobTests extends GrailsUnitTestCase
 
     void testFindEmbargoedReleasesToday()
     {
-        Calendar yesterday = Calendar.getInstance()
-        yesterday.add(Calendar.DAY_OF_YEAR, -1)
-        Calendar tomorrow = Calendar.getInstance()
-        tomorrow.add(Calendar.DAY_OF_YEAR, 1)
-
-        Tag tag = new Tag(codeMap:new CodeMap(codeMap:'A69-1303'), pingCode:1234)
-        mockDomain(Tag, [tag])
-        tag.save()
-        
-        AnimalRelease nullRelease = new AnimalRelease()
-        AnimalRelease yesterdayRelease = new AnimalRelease(embargoDate: yesterday.getTime())
-        AnimalRelease todayRelease = new AnimalRelease(embargoDate: new Date())
-        AnimalRelease tomorrowRelease = new AnimalRelease(embargoDate: tomorrow.getTime())
-        
-        def releases = [nullRelease, yesterdayRelease, todayRelease, tomorrowRelease]
-        mockDomain(AnimalRelease, releases)
-        
-        nullRelease.addToSurgeries(new Surgery(release:nullRelease, tag:tag))
-        yesterdayRelease.addToSurgeries(new Surgery(release:yesterdayRelease, tag:tag))
-        todayRelease.addToSurgeries(new Surgery(release:todayRelease, tag:tag))
-        tomorrowRelease.addToSurgeries(new Surgery(release:tomorrowRelease, tag:tag))
-        
-        releases.each{ it.save() }
-        
-        def releaseCriteria = [list: {Closure cls -> [yesterdayRelease, todayRelease, tomorrowRelease]}]
-        AnimalRelease.metaClass.static.createCriteria = {releaseCriteria}
-        
         def embargoedReleases = job.findEmbargoedReleases(0)
         assertEquals([todayRelease], embargoedReleases)
     }
+	
+	void testExpiringSubject()
+	{
+		assertEquals("AATAMS tag embargoes expiring today: [A69-1303-1234]", job.createExpiringSubject(todayRelease))
+	}
+		
+	void testExpiringBody()
+	{
+		String expectedBody = '''The embargoes on the following tags will expire today.
+
+A69-1303-1234
+
+http://localhost:8090/aatams/animalRelease/show/''' + todayRelease.id
+
+		assertEquals(expectedBody, job.createExpiringBody(todayRelease))
+	}
 }

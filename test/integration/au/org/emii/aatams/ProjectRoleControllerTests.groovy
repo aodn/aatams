c2111a154b6b69a0fa76b03ca458537f9dd08dec
Jon Burgess <jkburges@gmail.com>
2012-02-02 02:47:41 +0000

    1. #951 - making appropriate calls in to permission utils service when updating existing role.

--- test/integration/au/org/emii/aatams/ProjectRoleControllerTests.groovy
+++ test/integration/au/org/emii/aatams/ProjectRoleControllerTests.groovy
@@ -1,71 +1,148 @@
 package au.org.emii.aatams
 
-import au.org.emii.aatams.test.AbstractControllerUnitTestCase
+import org.joda.time.DateTimeZone;
 
 import grails.test.*
 import grails.converters.JSON
 
-class ProjectRoleControllerTests extends AbstractControllerUnitTestCase 
+class ProjectRoleControllerTests extends ControllerUnitTestCase 
 {
 	def permissionUtilsService
 	def searchableService
 	def sessionFactory
 	
+	Person fredSmith
+	Project sealCount
+	ProjectRoleType administrator
+		
     protected void setUp() 
 	{
         super.setUp()
 		
 		controller.metaClass.message = {}
 		controller.permissionUtilsService = permissionUtilsService
+		
+		fredSmith = new Person(username: "fsmith",
+							   passwordHash: "asdf",
+							   name: "Fred Smith", 
+							   emailAddress: "fsmith@asdf.com", 
+							   phoneNumber:"1234", 
+							   status: EntityStatus.ACTIVE, 
+							   defaultTimeZone: DateTimeZone.forID("Australia/Hobart"),
+							   organisation: Organisation.findByName("CSIRO"))
+		fredSmith.save(failOnError:true)
+		
+		assertNotNull(fredSmith)
+		
+		sealCount = Project.findByName("Seal Count")
+		assertNotNull(sealCount)
+		
+		administrator = ProjectRoleType.findByDisplayName("Administrator")
+		assertNotNull(administrator)
     }
 
     protected void tearDown() 
 	{
+		fredSmith.delete()
+		
         super.tearDown()
     }
 
     void testSave() 
 	{
-		Person joeBloggs = Person.findByUsername("jbloggs")
-		assertNotNull(joeBloggs)
+		assertNoPermissions()
+		def projectRole = saveRole(administrator)
+		assertAdministratorPermissions()
+	
+		deleteRole(projectRole)
+    }
+
+	void testUpdate()
+	{
+		assertNoPermissions()
+		def projectRole = saveRole(administrator)
+		assertAdministratorPermissions()
 		
-		Project sealCount = Project.findByName("Seal Count")
-		assertNotNull(sealCount)
+		controller.params.id = projectRole.id
+		controller.params.person = projectRole.person
+		controller.params.project = projectRole.project
+		controller.params.roleType = ProjectRoleType.findByDisplayName(ProjectRoleType.PRINCIPAL_INVESTIGATOR)
+		controller.params.access = ProjectAccess.READ_WRITE
+		controller.update()
 		
-		ProjectRoleType administrator = ProjectRoleType.findByDisplayName("Administrator")
-		assertNotNull(administrator)
+		assertProjectInvestigatorPermissions()
 		
-		ProjectAccess readWriteAccess = ProjectAccess.READ_WRITE
-		
-		controller.params.person = joeBloggs
+		deleteRole(projectRole)
+	}
+
+	private def saveRole(roleType) 
+	{
+		controller.params.person = fredSmith
 		controller.params.project = sealCount
-		controller.params.roleType = administrator
-		controller.params.access = readWriteAccess
-		
-		beforeFilter()
+		controller.params.roleType = roleType
+		controller.params.access = ProjectAccess.READ_WRITE
+
 		controller.save()
-		afterFilter()
 		
 		def jsonResponse = JSON.parse(controller.response.contentAsString)
 		
 		assertNotNull(jsonResponse.instance)
-		def projectRoleId = jsonResponse.instance.id
+		def projectRole = ProjectRole.get(jsonResponse.instance.id)
+		assertNotNull(projectRole)
 		
-		controller.params.id = projectRoleId
-		
-		beforeFilter()
-		controller.delete()
-		afterFilter()
-    }
-	
-	private void beforeFilter()
+		return projectRole
+	}
+
+	private void deleteRole(projectRole) 
 	{
-		searchableService.stopMirroring()
+		controller.params.id = projectRole.id
+		controller.delete()
+		assertNoPermissions()
+	}
+
+	private void assertProjectInvestigatorPermissions() 
+	{
+		assertPermissions(
+				[permissionUtilsService.buildProjectWritePermission(sealCount.id),
+					permissionUtilsService.buildProjectReadPermission(sealCount.id),
+					permissionUtilsService.PROJECT_WRITE_ANY,
+					permissionUtilsService.PROJECT_READ_ANY,
+					permissionUtilsService.PERSON_WRITE_ANY,
+					permissionUtilsService.RECEIVER_CREATE],
+				[])
+	}
+
+	private void assertAdministratorPermissions()
+	{
+		assertPermissions([permissionUtilsService.buildProjectWritePermission(sealCount.id),
+						   permissionUtilsService.buildProjectReadPermission(sealCount.id),
+						   permissionUtilsService.PROJECT_WRITE_ANY,
+						   permissionUtilsService.PROJECT_READ_ANY],
+					      [permissionUtilsService.PERSON_WRITE_ANY,
+						   permissionUtilsService.RECEIVER_CREATE])
 	}
 	
-	private void afterFilter()
+	private void assertNoPermissions() 
 	{
-		sessionFactory.getCurrentSession().flush()
-		searchableService.startMirroring()
+		assertPermissions([],
+		[permissionUtilsService.buildProjectWritePermission(sealCount.id),
+			permissionUtilsService.buildProjectReadPermission(sealCount.id),
+			permissionUtilsService.PROJECT_WRITE_ANY,
+			permissionUtilsService.PROJECT_READ_ANY,
+			permissionUtilsService.PERSON_WRITE_ANY,
+			permissionUtilsService.RECEIVER_CREATE])
+	}
+	
+	private void assertPermissions(truePermissions, falsePermissions)
+	{
+		truePermissions.each
+		{
+			assertTrue(it, fredSmith.permissions.contains(it))
+		}
+		
+		falsePermissions.each
+		{
+			assertFalse(it, fredSmith.permissions.contains(it))
+		}
 	}
 }

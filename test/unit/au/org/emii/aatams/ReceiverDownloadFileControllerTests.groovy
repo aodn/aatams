9a1eaf6fa9a892cd8ae09f8b38bc7389f498c32b
Jon Burgess <jkburges@gmail.com>
2012-03-26 06:21:13 +0000

    #1160 - refreshing the detections materialized view after every successful upload.

--- test/unit/au/org/emii/aatams/ReceiverDownloadFileControllerTests.groovy
+++ test/unit/au/org/emii/aatams/ReceiverDownloadFileControllerTests.groovy
@@ -1,10 +1,12 @@
 package au.org.emii.aatams
 
 import grails.test.*
+import org.springframework.jdbc.core.JdbcTemplate
 
 class ReceiverDownloadFileControllerTests extends ControllerUnitTestCase 
 {
-    protected void setUp() {
+    protected void setUp() 
+	{
         super.setUp()
     }
 
@@ -12,9 +14,21 @@ class ReceiverDownloadFileControllerTests extends ControllerUnitTestCase
         super.tearDown()
     }
 
-    void testUnknownReceiver() 
-    {
-        // Marker - need to update the controller so that error message is given.
-//        fail()
-    }
+	void testRefreshDetectionCountPerStationView()
+	{
+		controller.metaClass.getRefreshStatement =
+		{
+			def ret = new JdbcTemplate()
+			ret.metaClass.execute =
+			{
+				String sql ->
+				
+				assertEquals('''SELECT refresh_matview('detection_count_per_station_mv');''', sql)
+			}
+
+			return ret		
+		}
+		
+		controller.refreshDetectionCountPerStationView()
+	}
 }

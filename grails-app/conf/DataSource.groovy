ebf2014024e53825520ce9d3e431cbe500d73ee4
Jon Burgess <jkburges@gmail.com>
2012-05-03 05:26:01 +0000

    - reverted accidental check-in.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -49,8 +49,7 @@ environments {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop','update'
             driverClassName = "org.postgresql.Driver"
-//            url = "jdbc:postgresql://dbtest.emii.org.au:5432/aatams_test?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
-            url = "jdbc:postgresql://localhost:5432/aatams_test"
+            url = "jdbc:postgresql://dbtest.emii.org.au:5432/aatams_test?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
             username = "aatams"
             password = "aatams"
 

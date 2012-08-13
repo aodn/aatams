10ec1405c3f1a29baa18e15da2256cde97cb6d68
Jon Burgess <jkburges@gmail.com>
2012-08-13 07:00:24 +0000

    #1066 - only authenticated users can access the audit log event list.

--- test/unit/SecSecurityFiltersTests.groovy
+++ test/unit/SecSecurityFiltersTests.groovy
@@ -32,6 +32,7 @@ class SecSecurityFiltersTests extends AbstractFiltersUnitTestCase
         "animalMeasurement",
         "animalMeasurementType",
         "animalRelease",
+		"auditLogEvent",
         "detection",
         "detectionSurgery",
         "device",

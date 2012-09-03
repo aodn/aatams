aa296bafa324788164ad2e12eb16270e8d6d487b
Jon Burgess <jkburges@gmail.com>
2012-09-03 05:03:18 +0000

    Updated SQL.

---
+++ scripts/sql/tims_reports/generate_report.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+HOST=$1
+DB=$2
+
+FILENAMES=('releases_by_species.sql' 'overall_stats.sql' 'detections_by_project.sql' 'detections_by_species.sql' 'installation_list.sql')
+
+for filename in "${FILENAMES[@]}"; do
+	echo "Executing file: $filename ...";
+    psql -h $HOST -d $DB -f $filename;
+    echo "";
+done;
+

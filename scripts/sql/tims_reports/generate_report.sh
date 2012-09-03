5124a9b7f91010961438f81c71076d3e87aad01f
jburgess <jburgess@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-09-03 05:28:17 +0000

    Added project listing.

--- scripts/sql/tims_reports/generate_report.sh
+++ scripts/sql/tims_reports/generate_report.sh
@@ -3,7 +3,7 @@
 HOST=$1
 DB=$2
 
-FILENAMES=('releases_by_species.sql' 'overall_stats.sql' 'detections_by_project.sql' 'detections_by_species.sql' 'installation_list.sql')
+FILENAMES=('project_list.sql' 'releases_by_species.sql' 'overall_stats.sql' 'detections_by_project.sql' 'detections_by_species.sql' 'installation_list.sql')
 
 for filename in "${FILENAMES[@]}"; do
 	echo "Executing file: $filename ...";

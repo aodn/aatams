f735163810fa5c7d0427cb11169dc9d82c0cf6bf
Jon Burgess <jkburges@gmail.com>
2012-09-10 06:58:56 +0000

    #1967 - checking for empty species id.

--- test/unit/au/org/emii/aatams/AnimalControllerTests.groovy
+++ test/unit/au/org/emii/aatams/AnimalControllerTests.groovy
@@ -2,16 +2,23 @@ package au.org.emii.aatams
 
 import grails.test.*
 
-class AnimalControllerTests extends ControllerUnitTestCase {
-    protected void setUp() {
+class AnimalControllerTests extends ControllerUnitTestCase 
+{
+    protected void setUp() 
+	{
         super.setUp()
     }
 
-    protected void tearDown() {
+    protected void tearDown() 
+	{
         super.tearDown()
     }
 
-    void testSomething() {
-
+    void testLookupAnimalWithEmptySpecies() 
+	{
+		controller.params.project = [id: ""]
+		controller.params.species = [id: ""]
+		
+		controller.lookup()
     }
 }

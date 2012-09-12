afe06f4922dfc5a478006a0deb3a58601d460b07
Jon Burgess <jkburges@gmail.com>
2012-09-12 01:14:31 +0000

    #1352 - apply autocomplete combobox to all selects (why not?)

--- web-app/js/autocompleteForLargeSelects.js
+++ web-app/js/autocompleteForLargeSelects.js
@@ -1,12 +1,6 @@
 $(function() {
    
-    var maxOptions = 10;
-    
     $("select").each(function() {
-       
-        if ($(this).children().size() > maxOptions) {
-            
-            $(this).combobox();
-        }
+        $(this).combobox();
     });
 });

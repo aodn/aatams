a24daa9231613c7c850d85f7ec66f2c9b511fcc8
Jon Burgess <jkburges@gmail.com>
2012-09-12 01:32:25 +0000

    #1352 - don't apply autocomplete to date/location drop downs.

--- web-app/js/autocompleteForLargeSelects.js
+++ web-app/js/autocompleteForLargeSelects.js
@@ -1,6 +1,28 @@
 $(function() {
    
     $("select").each(function() {
-        $(this).combobox();
+        
+        // Except for date/time fields...
+        if (!isDateField(this)) {
+            $(this).combobox();
+        }
     });
 });
+
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
+function isDateField(element) {
+    
+    var isDate = false;
+    
+    $(["day", "month", "year", "hour", "minute", "second", "editNorthSouth", "editEastWest"]).each(function(index, elementId) {
+        
+        if ($(element).attr("id").endsWith("_" + elementId)) {
+            isDate = true;
+        }
+    })
+    
+    return isDate;
+}

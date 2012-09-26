6a02ae5160b61685eb92c79d08505ccb2332e205
jburgess <jburgess@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-09-26 02:59:36 +0000

    Updated SQL in line with detection table split.

--- scripts/sql/tims_reports/detections_by_species.sql
+++ scripts/sql/tims_reports/detections_by_species.sql
@@ -1,18 +1,16 @@
-select count(*), species.common_name, species.scientific_name, species.spcode from raw_detection
-join detection_surgery on raw_detection.id = detection_surgery.id
+select count(*), species.common_name, species.scientific_name, species.spcode from valid_detection
+join detection_surgery on valid_detection.id = detection_surgery.detection_id
 join surgery on detection_surgery.surgery_id = surgery.id
 join animal_release on surgery.release_id = animal_release.id
 join animal on animal_release.animal_id = animal.id
 join species on animal.species_id = species.id
-where raw_detection.class = 'au.org.emii.aatams.detection.ValidDetection'
 group by species.spcode, species.scientific_name, species.common_name
 order by count (*) desc;
 
-select count(*) as "detections by species" from raw_detection
-join detection_surgery on raw_detection.id = detection_surgery.id
+select count(*) as "detections by species" from valid_detection
+join detection_surgery on valid_detection.id = detection_surgery.detection_id
 join surgery on detection_surgery.surgery_id = surgery.id
 join animal_release on surgery.release_id = animal_release.id
 join animal on animal_release.animal_id = animal.id
 join species on animal.species_id = species.id
---where raw_detection.class = 'au.org.emii.aatams.detection.ValidDetection'
 order by count (*) desc;
--- scripts/sql/tims_reports/overall_stats.sql
+++ scripts/sql/tims_reports/overall_stats.sql
@@ -1,24 +1,22 @@
 -- num unique transmitter_ids
-select count(distinct transmitter_id) as "num unique tag IDs detected" from raw_detection where class = 'au.org.emii.aatams.detection.ValidDetection';
+select count(distinct transmitter_id) as "num unique tag IDs detected" from valid_detection;
 
 -- num tags
 select count (*) as "tags AATAMS knows about" from device where class = 'au.org.emii.aatams.Tag';
 
 -- num known detected tag count
-select count(distinct sensor.transmitter_id) as "num detected tags AATAMS knows about" from raw_detection
-join sensor on raw_detection.transmitter_id = sensor.transmitter_id
-where raw_detection.class = 'au.org.emii.aatams.detection.ValidDetection';
+select count(distinct sensor.transmitter_id) as "num detected tags AATAMS knows about" from valid_detection
+join sensor on valid_detection.transmitter_id = sensor.transmitter_id;
 
 -- unique tags detected by species
-select count(distinct tag_id) as "tags detected by species" from raw_detection
-join detection_surgery on raw_detection.id = detection_surgery.id
-join surgery on detection_surgery.surgery_id = surgery.id
-where raw_detection.class = 'au.org.emii.aatams.detection.ValidDetection';
+select count(distinct tag_id) as "tags detected by species" from valid_detection
+join detection_surgery on valid_detection.id = detection_surgery.detection_id
+join surgery on detection_surgery.surgery_id = surgery.id;
 
 
 select count (*) release_count from animal_release;
 
-select count(*) as detection_count from raw_detection where class = 'au.org.emii.aatams.detection.ValidDetection';
+select count(*) as detection_count from valid_detection;
 
 select count(*) as deployment_count from receiver_deployment;
 

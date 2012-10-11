a3a8deb424246050c737ed2ceb94122769308fb1
Jon Burgess <jkburges@gmail.com>
2012-10-11 06:27:54 +0000

    Trying "merge" again.

--- scripts/sql/tims_reports/detections_by_project.sql
+++ scripts/sql/tims_reports/detections_by_project.sql
@@ -1,9 +1,8 @@
-select count(project.name), project.name from raw_detection
-join receiver_deployment on raw_detection.receiver_deployment_id = receiver_deployment.id
+select count(project.name), project.name from valid_detection
+join receiver_deployment on valid_detection.receiver_deployment_id = receiver_deployment.id
 join installation_station on receiver_deployment.station_id = installation_station.id
 join installation on installation_station.installation_id = installation.id
 join project on installation.project_id = project.id
-where class = 'au.org.emii.aatams.detection.ValidDetection'
 group by project.name
 order by count(project.name) desc;
 

3c1492946a35f4fe4dba679a7423432cdb6e32a1
jburgess <jburgess@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-10-11 03:25:43 +0000

    Added script to create views in database.

--- scripts/sql/tims_reports/detections_by_species.sql
+++ scripts/sql/tims_reports/detections_by_species.sql
@@ -7,10 +7,10 @@ join species on animal.species_id = species.id
 group by species.spcode, species.scientific_name, species.common_name
 order by count (*) desc;
 
-select count(*) as "detections by species" from valid_detection
-join detection_surgery on valid_detection.id = detection_surgery.detection_id
-join surgery on detection_surgery.surgery_id = surgery.id
-join animal_release on surgery.release_id = animal_release.id
-join animal on animal_release.animal_id = animal.id
-join species on animal.species_id = species.id
-order by count (*) desc;
+--select count(*) as "detections by species" from valid_detection
+--join detection_surgery on valid_detection.id = detection_surgery.detection_id
+--join surgery on detection_surgery.surgery_id = surgery.id
+--join animal_release on surgery.release_id = animal_release.id
+--join animal on animal_release.animal_id = animal.id
+--join species on animal.species_id = species.id
+--order by count (*) desc;

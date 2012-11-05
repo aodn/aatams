6cdae0cc5f7eff57851bcfc2816f3199de37ece6
Jon Burgess <jkburges@gmail.com>
2012-11-05 05:03:35 +0000

    #1751 - deleting duplicate detections in migration - rescanning for surgeries matching promoted detections.

--- grails-app/services/au/org/emii/aatams/detection/DetectionFactoryService.groovy
+++ grails-app/services/au/org/emii/aatams/detection/DetectionFactoryService.groovy
@@ -11,7 +11,10 @@ import com.vividsolutions.jts.geom.Point;
 import org.joda.time.*
 import org.joda.time.format.ISODateTimeFormat;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
+import java.sql.ResultSet
+import java.sql.SQLException
 import java.util.Date;
 import java.util.TimeZone
 
@@ -95,11 +98,15 @@ class DetectionFactoryService
         return newDetectionSurgeries
     }
 	
+	private String getDetectionsForDeploymentCondition(deployment)
+	{
+		def dateTimeFormatter = ISODateTimeFormat.dateTimeNoMillis()
+		return "receiver_name = '${deployment.receiver.name}' and timestamp between '${dateTimeFormatter.print(deployment.deploymentDateTime)}' AND '${dateTimeFormatter.print(deployment.recovery.recoveryDateTime)}'"
+	}
 	
 	private String buildRescanDeploymentSql(deployment)
 	{
-		def dateTimeFormatter = ISODateTimeFormat.dateTimeNoMillis()
-		def condition = "reason <> 'DUPLICATE' and receiver_name = '${deployment.receiver.name}' and timestamp between '${dateTimeFormatter.print(deployment.deploymentDateTime)}' AND '${dateTimeFormatter.print(deployment.recovery.recoveryDateTime)}'"
+		def condition = "reason <> 'DUPLICATE' and ${getDetectionsForDeploymentCondition(deployment)}"
 		def validDetectionColumnNames = "id, version, location, receiver_deployment_id, receiver_download_id, receiver_name, sensor_unit, sensor_value, station_name, \
 					 timestamp, transmitter_id, transmitter_name, transmitter_serial_number"
 		def invalidDetectionSelectFields = "id, version, location, ${deployment.id}, receiver_download_id, receiver_name, sensor_unit, sensor_value, station_name, timestamp, transmitter_id, transmitter_name, transmitter_serial_number"
@@ -124,6 +131,59 @@ class DetectionFactoryService
 		JdbcTemplate rescanInvalid = new JdbcTemplate(dataSource)
 		rescanInvalid.execute(buildRescanDeploymentSql(deployment))
 		log.info("Rescan complete, deployment: ${deployment}")
+		
+		log.info("Rescanning surgeries...")
+		JdbcTemplate rescanSurgeries = new JdbcTemplate(dataSource)
+		def rows = 
+			rescanSurgeries.query(
+				"select id, timestamp, transmitter_id from valid_detection where ${getDetectionsForDeploymentCondition(deployment)}", 
+				new RowMapper()
+				{
+					public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+						Map retRow = [:]
+						
+						retRow.id = rs.getLong("id")
+						retRow.timestamp = rs.getDate("timestamp")
+						retRow.transmitter_id = rs.getString("transmitter_id")
+						return retRow
+					}
+				})
+		
+		log.info("Rescanning surgeries for ${rows.size()} detections...")
+		
+		int progress = 0
+		int prevProgress = -1
+				
+		rows.eachWithIndex 
+		{
+			detection, index ->
+			
+			if (DetectionSurgery.findByDetection(ValidDetection.get(detection.id)))
+			{
+				// duplicate
+			}
+			else
+			{
+				def sensor = Sensor.findByTransmitterId(detection.transmitter_id, [cache: true])
+				
+				Surgery.findAllByTag(sensor.tag).each 
+				{
+					surgery ->
+					
+					if (surgery.isInWindow(detection.timestamp))
+					{
+						createDetectionSurgery(surgery, sensor, ValidDetection.get(detection.id))
+					}
+				}
+			}
+			
+			progress = ((float) index / rows.size()) * 100
+			if (progress != prevProgress)
+			{
+				prevProgress = progress
+				log.info("${progress}% of surgeries rescanned")
+			}
+		}
 	}
     
 	private int logProgress(i, numRecords, percentProgress, surgery, startTime)

8762dc93fc80dd814304302d1ca98cfe9302a9dc
Tommy Fotak <tommy.fotak@gmail.com>
2013-08-23 14:57:35 +1000

    Use Java style exceptions for JDK7
    There is a change in the number of constructor args or something for Exception
    between JDK6 and JDK7, using Java style constructors for Exception classes
    means that you can run on JDK7 without errors see
    http://blog.proxerd.pl/article/how-to-fix-incompatibleclasschangeerror-for-your-groovy-projects-running-on-jdk7

--- src/groovy/au/org/emii/aatams/bulk/BulkImportException.groovy
+++ src/groovy/au/org/emii/aatams/bulk/BulkImportException.groovy
@@ -2,22 +2,22 @@ package au.org.emii.aatams.bulk
 
 class BulkImportException extends Exception
 {
-    BulkImportException()
+    public BulkImportException()
     {
         super()
     }
 
-    BulkImportException(String message)
+    public BulkImportException(String message)
     {
         super(message)
     }
 
-    BulkImportException(String message, Throwable cause)
+    public BulkImportException(String message, Throwable cause)
     {
         super(message, cause)
     }
-    
-    BulkImportException(Throwable cause)
+
+    public BulkImportException(Throwable cause)
     {
         super(cause)
     }

85171a59509454c973f58f1ab733948e4265a4cd
Jon Burgess <jkburges@gmail.com>
2013-09-25 11:14:41 +1000

    Replaced tabs with spaces.

--- grails-app/migrations/foreign_key_indices.groovy
+++ grails-app/migrations/foreign_key_indices.groovy
@@ -1,26 +1,26 @@
 databaseChangeLog = {
 
     changeSet(author: "jburgess", id: "1380074275000-01") {
-		createIndex(indexName: "valid_detection_receiver_deployment_index", tableName: "valid_detection", unique: "false") {
-			column(name: "receiver_deployment_id")
-		}
-	}
+        createIndex(indexName: "valid_detection_receiver_deployment_index", tableName: "valid_detection", unique: "false") {
+            column(name: "receiver_deployment_id")
+        }
+    }
 
     changeSet(author: "jburgess", id: "1380074275000-02") {
-		createIndex(indexName: "receiver_deployment_installation_station_id_index", tableName: "receiver_deployment", unique: "false") {
-			column(name: "station_id")
-		}
-	}
+        createIndex(indexName: "receiver_deployment_installation_station_id_index", tableName: "receiver_deployment", unique: "false") {
+            column(name: "station_id")
+        }
+    }
 
     changeSet(author: "jburgess", id: "1380074275000-03") {
-		createIndex(indexName: "installation_station_installation_id_index", tableName: "installation_station", unique: "false") {
-			column(name: "installation_id")
-		}
-	}
+        createIndex(indexName: "installation_station_installation_id_index", tableName: "installation_station", unique: "false") {
+            column(name: "installation_id")
+        }
+    }
 
     changeSet(author: "jburgess", id: "1380074275000-04") {
-		createIndex(indexName: "installation_project_id_index", tableName: "installation", unique: "false") {
-			column(name: "project_id")
-		}
-	}
+        createIndex(indexName: "installation_project_id_index", tableName: "installation", unique: "false") {
+            column(name: "project_id")
+        }
+    }
 }

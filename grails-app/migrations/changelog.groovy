59e6f4b2ff3ab01745a6b4bcc775f1f3cc440767
Jon Burgess <jkburges@gmail.com>
2013-09-25 13:47:03 +1000

    Updated view to get URL from the config (rather than incorrect hardcoded value).

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -2,4 +2,5 @@ databaseChangeLog = {
     include file: 'rebased_db_initialisation.groovy'
     include file: 'missing_indices.groovy'
     include file: 'foreign_key_indices.groovy'
+    include file: 'correct_url_in_detection_count_per_station.groovy'
 }

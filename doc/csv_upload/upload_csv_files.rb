dcd626e5e93dd363c7aeda78161c637f6a3ba812
Jon Burgess <jkburges@gmail.com>
2014-07-28 10:22:38 +1000

    Refactor.  Replaced a few comments with functions.

--- doc/csv_upload/upload_csv_files.rb
+++ doc/csv_upload/upload_csv_files.rb
@@ -9,35 +9,47 @@ require 'nokogiri'
 # $ grep -Rl "Transmitter" * | ~/git/aatams/doc/csv_upload/upload_csv_files.rb
 #
 
-AATAMS_URL = ENV['AATAMS_URL'] || "http://localhost:8080/aatams"
+$AATAMS_URL = ENV['AATAMS_URL'] || "http://localhost:8080/aatams"
 
-http = HTTPClient.new
+$http = HTTPClient.new
 
-# Authenticate
-http.post "#{AATAMS_URL}/auth/signIn", :username => 'jkburges', :password => 'password'
+def authenticate
+  $http.post "#{$AATAMS_URL}/auth/signIn", :username => 'jkburges', :password => 'password'
+end
 
-ARGF.each_line { |upload_file_path|
+def upload_files
+  ARGF.each_line { |upload_file_path|
+    upload_file upload_file_path
+  }
+end
 
-  # Upload file.
+def upload_file(upload_file_path)
   puts "Uploading file: #{upload_file_path}"
 
   upload_file = File.new(upload_file_path.chomp)
 
-  response = http.post "#{AATAMS_URL}/receiverDownloadFile/save?format=xml",
-    :type => 'DETECTIONS_CSV',
-    File.basename(upload_file_path) => upload_file
+  response = $http.post "#{$AATAMS_URL}/receiverDownloadFile/save?format=xml",
+  :type => 'DETECTIONS_CSV',
+  File.basename(upload_file_path) => upload_file
 
-  show_upload_url = response.headers['Location']
+  wait_for_processing_to_end response.headers['Location']
+end
 
-
-  # Wait for processing to finish.
+def wait_for_processing_to_end(show_upload_url)
   status = 'PROCESSING'
 
   while status == 'PROCESSING' do
     sleep 2
-    upload_response = Nokogiri::XML(http.get(show_upload_url).content)
+    upload_response = Nokogiri::XML($http.get(show_upload_url).content)
     statusAttr = upload_response.xpath('//receiverDownloadFile/@status')[0]
 
     status = statusAttr.to_str
   end
-}
+end
+
+def main
+  authenticate
+  upload_files
+end
+
+main

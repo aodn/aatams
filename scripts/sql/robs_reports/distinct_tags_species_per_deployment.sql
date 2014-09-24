9898f089a721b875730c92e042dee3998a11317b
Jon Burgess <jkburges@gmail.com>
2014-09-25 09:26:04 +1000

    Include both bottom depth and depth below surface, to avoid confusion (?)

--- scripts/sql/robs_reports/distinct_tags_species_per_deployment.sql
+++ scripts/sql/robs_reports/distinct_tags_species_per_deployment.sql
@@ -8,7 +8,8 @@ COPY (
         count(distinct transmitter_id) as distinct_tag_count,
         array_to_string(array_agg(distinct transmitter_id), ',') as tag_ids,
         st_y(receiver_deployment.location) as latitude, st_x(receiver_deployment.location) as longitude,
-        depth_below_surfacem, deploymentdatetime_timestamp as deployment_datetime
+        depth_below_surfacem, bottom_depthm,
+        deploymentdatetime_timestamp as deployment_datetime
 
     from receiver_deployment
     join valid_detection on valid_detection.receiver_deployment_id = receiver_deployment.id
@@ -20,6 +21,6 @@ COPY (
     left join animal on animal.id = animal_release.animal_id
     left join species on species.id = animal.species_id
 
-    group by installation.name, installation_station.name, species.common_name, latitude, longitude, depth_below_surfacem, deploymentdatetime_timestamp
+    group by installation.name, installation_station.name, species.common_name, latitude, longitude, depth_below_surfacem, bottom_depthm, deploymentdatetime_timestamp
     order by installation.name, installation_station.name, species.common_name
 ) TO stdout WITH CSV HEADER;

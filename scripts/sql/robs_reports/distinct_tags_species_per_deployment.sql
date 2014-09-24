e983cbe15fc354f508f0b213d9c4d1568ccd54da
Jon Burgess <jkburges@gmail.com>
2014-09-24 14:43:30 +1000

    The last attempt was taking too long (4 hours and counting).  Perhaps this will be faster.

--- scripts/sql/robs_reports/distinct_tags_species_per_deployment.sql
+++ scripts/sql/robs_reports/distinct_tags_species_per_deployment.sql
@@ -2,14 +2,23 @@ set search_path to 'aatams', 'public';
 
 COPY (
     select distinct
-        installation,
-        station,
-        species_name,
-        count(distinct dets.transmitter_id) as distinct_tag_count,
-        latitude, longitude, depth_below_surfacem, deploymentdatetime_timestamp as deployment_datetime
-    from (select * from detection_extract_view_mv) as dets
-    join valid_detection on detection_id = valid_detection.id
-    join receiver_deployment on valid_detection.receiver_deployment_id = receiver_deployment.id
-    group by installation, station, species_name, latitude, longitude, depth_below_surfacem, deploymentdatetime_timestamp
-    order by installation, station, species_name
+        installation.name as installation_name,
+        installation_station.name as station_name,
+        species.common_name,
+        count(distinct transmitter_id) as distinct_tag_count,
+        st_y(receiver_deployment.location) as latitude, st_x(receiver_deployment.location) as longitude,
+        depth_below_surfacem, deploymentdatetime_timestamp as deployment_datetime
+
+    from receiver_deployment
+    join valid_detection on valid_detection.receiver_deployment_id = receiver_deployment.id
+    join installation_station on installation_station.id = receiver_deployment.station_id
+    join installation on installation.id = installation_station.installation_id
+    left join detection_surgery on detection_surgery.detection_id = valid_detection.id
+    left join surgery on surgery.id = detection_surgery.surgery_id
+    left join animal_release on animal_release.id = surgery.release_id
+    left join animal on animal.id = animal_release.animal_id
+    left join species on species.id = animal.species_id
+
+    group by installation.name, installation_station.name, species.common_name, latitude, longitude, depth_below_surfacem, deploymentdatetime_timestamp
+    order by installation.name, installation_station.name, species.common_name
 ) TO stdout WITH CSV HEADER;

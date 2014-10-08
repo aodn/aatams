ed32752bfc08e56fd5229b8bf00edf6707768609
David Nahodil <dnahodil@utas.edu.au>
2014-10-09 10:20:58 +1100

    Whitespace

--- test/integration/au/org/emii/aatams/detection/JdbcTemplateVueDetectionFileProcessorServiceIntegrationTests.groovy
+++ test/integration/au/org/emii/aatams/detection/JdbcTemplateVueDetectionFileProcessorServiceIntegrationTests.groovy
@@ -1,21 +1,16 @@
 package au.org.emii.aatams.detection
 
 import au.org.emii.aatams.*
-import au.org.emii.aatams.test.*
 
-import grails.test.*
-import groovy.sql.*
-import groovy.lang.MetaClass
-
-class JdbcTemplateVueDetectionFileProcessorServiceIntegrationTests extends AbstractJdbcTemplateVueDetectionFileProcessorServiceIntegrationTests 
+class JdbcTemplateVueDetectionFileProcessorServiceIntegrationTests extends AbstractJdbcTemplateVueDetectionFileProcessorServiceIntegrationTests
 {
     ReceiverDownloadFile export
     def exportFile
 
-    protected void setUp() 
+    protected void setUp()
     {
         super.setUp()
-        
+
         export =
             new ReceiverDownloadFile(type: ReceiverDownloadFileType.DETECTIONS_CSV,
             name: "duplicate",
@@ -23,38 +18,38 @@ class JdbcTemplateVueDetectionFileProcessorServiceIntegrationTests extends Abstr
             status: FileProcessingStatus.PROCESSING,
             errMsg: "",
             requestingUser: null).save(failOnError:true)
-                                         
+
             export.initialiseForProcessing("duplicate.csv")
             export.requestingUser = Person.findByUsername('jbloggs')
-            export.save(flush: true, failOnError: true)                    
-        
+            export.save(flush: true, failOnError: true)
+
         export = ReceiverDownloadFile.read(export.id)
-        
+
         exportFile = new File(export.path)
         exportFile.getParentFile().mkdirs()
         exportFile << '''Date and Time (UTC),Receiver,Transmitter,Transmitter Name,Transmitter Serial,Sensor Value,Sensor Unit,Station Name,Latitude,Longitude
 '''
     }
 
-    protected void tearDown() 
+    protected void tearDown()
     {
         exportFile?.delete()
         getRefreshedExport(export)?.delete()
-        
+
         super.tearDown()
     }
-    
+
     // Test for #2055
     void testNothing()
     {
-        
+
     }
 
-    void testNoDetectionSurgeryForDuplicateDetection() 
+    void testNoDetectionSurgeryForDuplicateDetection()
     {
         exportFile << '''2011-05-17 02:54:05,VR2W-101336,A69-1303-62339
 '''
-        
+
         def detSurgeryCount = DetectionSurgery.count()
         jdbcTemplateVueDetectionFileProcessorService.process(export)
         assertEquals(detSurgeryCount, DetectionSurgery.count())
@@ -68,14 +63,14 @@ class JdbcTemplateVueDetectionFileProcessorServiceIntegrationTests extends Abstr
         assertEquals(origMatViewCount, origValidDetCount)
 
         def origStatisticsNumValidDetCount = Statistics.findByKey('numValidDetections')?.value
-            
+
         def detRows = ['2011-05-17 03:54:05,VR2W-101336,A69-1303-12345','2011-05-17 04:54:05,VR2W-101336,A69-1303-12345', '2011-05-17 05:54:05,VR2W-101336,A69-1303-12345']
         def numNewDets = detRows.size()
-            
+
         exportFile << detRows.join('\n')
 
         jdbcTemplateVueDetectionFileProcessorService.process(export)
-            
+
         def finalMatViewCount = sql.firstRow('select count(*) from detection_extract_view_mv;').count
         def finalValidDetCount = sql.firstRow('select count(*) from valid_detection;').count
         def finalProvDetCount = ValidDetection.findAllByProvisional(true).size()
@@ -87,7 +82,7 @@ class JdbcTemplateVueDetectionFileProcessorServiceIntegrationTests extends Abstr
         assertEquals(origValidDetCount + numNewDets, finalValidDetCount)
         assertEquals(origStatisticsNumValidDetCount + numNewDets, finalStatisticsNumValidDetCount)
     }
-    
+
     private def getRefreshedExport(export)
     {
         return ReceiverDownloadFile.read(export?.id)

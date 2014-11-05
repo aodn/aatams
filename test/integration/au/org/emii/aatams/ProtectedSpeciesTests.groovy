717497b25ee1516273e445252553521fc043e505
David Nahodil <dnahodil@utas.edu.au>
2014-11-06 10:58:07 +1100

    Sanitisation behaviour

--- test/integration/au/org/emii/aatams/ProtectedSpeciesTests.groovy
+++ test/integration/au/org/emii/aatams/ProtectedSpeciesTests.groovy
@@ -43,33 +43,99 @@ class ProtectedSpeciesTests extends AbstractJdbcTemplateVueDetectionFileProcesso
         controller = new DetectionController()
     }
 
-    void testProtectedSpeciesFiltering() {
+    void testProtectedSpeciesFilteringA() {
         assertVisible(UNAUTHENTICATED, UNEMBARGOED, FILTER_NOT_SET)
+    }
+
+    void testProtectedSpeciesFilteringB() {
         assertVisible(UNAUTHENTICATED, UNEMBARGOED, FILTER_SET)
+    }
+
+    void testProtectedSpeciesFilteringC() {
         assertVisibleButSanitised(UNAUTHENTICATED, EMBARGOED, FILTER_NOT_SET)
-        // Failing: assertNotVisible(UNAUTHENTICATED, EMBARGOED, FILTER_SET)
-        // Failing: assertNotVisible(UNAUTHENTICATED, PROTECTED, FILTER_NOT_SET)
-        // Failing: assertNotVisible(UNAUTHENTICATED, PROTECTED, FILTER_SET)
+    }
 
+    void testProtectedSpeciesFilteringD() {
+        assertNotVisible(UNAUTHENTICATED, EMBARGOED, FILTER_SET)
+    }
+
+    void testProtectedSpeciesFilteringE() {
+        assertNotVisible(UNAUTHENTICATED, PROTECTED, FILTER_NOT_SET)
+    }
+
+    void testProtectedSpeciesFilteringF() {
+        assertNotVisible(UNAUTHENTICATED, PROTECTED, FILTER_SET)
+    }
+
+    void testProtectedSpeciesFilteringG() {
         assertVisible(NON_PROJECT_MEMBER, UNEMBARGOED, FILTER_NOT_SET)
+    }
+
+    void testProtectedSpeciesFilteringH() {
         assertVisible(NON_PROJECT_MEMBER, UNEMBARGOED, FILTER_SET)
+    }
+
+    void testProtectedSpeciesFilteringI() {
         assertVisibleButSanitised(NON_PROJECT_MEMBER, EMBARGOED, FILTER_NOT_SET)
-        // Failing: assertNotVisible(NON_PROJECT_MEMBER, EMBARGOED, FILTER_SET)
-        // Failing: assertNotVisible(NON_PROJECT_MEMBER, PROTECTED, FILTER_NOT_SET)
-        // Failing: assertNotVisible(NON_PROJECT_MEMBER, PROTECTED, FILTER_SET)
+    }
 
+    void testProtectedSpeciesFilteringJ() {
+        assertNotVisible(NON_PROJECT_MEMBER, EMBARGOED, FILTER_SET)
+    }
+
+    void testProtectedSpeciesFilteringK() {
+        assertNotVisible(NON_PROJECT_MEMBER, PROTECTED, FILTER_NOT_SET)
+    }
+
+    void testProtectedSpeciesFilteringL() {
+        assertNotVisible(NON_PROJECT_MEMBER, PROTECTED, FILTER_SET)
+    }
+
+    void testProtectedSpeciesFilteringM() {
         assertVisible(PROJECT_MEMBER, UNEMBARGOED, FILTER_NOT_SET)
-        assertVisible(PROJECT_MEMBER, UNEMBARGOED, FILTER_SET)
-        assertVisible(PROJECT_MEMBER, EMBARGOED, FILTER_NOT_SET)
-        assertVisible(PROJECT_MEMBER, EMBARGOED, FILTER_SET)
-        assertVisible(PROJECT_MEMBER, PROTECTED, FILTER_NOT_SET)
-        assertVisible(PROJECT_MEMBER, PROTECTED, FILTER_SET)
+    }
 
+    void testProtectedSpeciesFilteringO() {
+        assertVisible(PROJECT_MEMBER, UNEMBARGOED, FILTER_SET)
+    }
+
+    void testProtectedSpeciesFilteringP() {
+        assertVisible(PROJECT_MEMBER, EMBARGOED, FILTER_NOT_SET)
+    }
+
+    void testProtectedSpeciesFilteringQ() {
+        assertVisible(PROJECT_MEMBER, EMBARGOED, FILTER_SET)
+    }
+
+    void testProtectedSpeciesFilteringR() {
+        assertVisible(PROJECT_MEMBER, PROTECTED, FILTER_NOT_SET)
+    }
+
+    void testProtectedSpeciesFilteringS() {
+        assertVisible(PROJECT_MEMBER, PROTECTED, FILTER_SET)
+    }
+
+    void testProtectedSpeciesFilteringT() {
         assertVisible(SYS_ADMIN, UNEMBARGOED, FILTER_NOT_SET)
+    }
+
+    void testProtectedSpeciesFilteringU() {
         assertVisible(SYS_ADMIN, UNEMBARGOED, FILTER_SET)
+    }
+
+    void testProtectedSpeciesFilteringV() {
         assertVisible(SYS_ADMIN, EMBARGOED, FILTER_NOT_SET)
+    }
+
+    void testProtectedSpeciesFilteringW() {
         assertVisible(SYS_ADMIN, EMBARGOED, FILTER_SET)
+    }
+
+    void testProtectedSpeciesFilteringX() {
         assertVisible(SYS_ADMIN, PROTECTED, FILTER_NOT_SET)
+    }
+
+    void testProtectedSpeciesFilteringY() {
         assertVisible(SYS_ADMIN, PROTECTED, FILTER_SET)
     }
 
@@ -91,29 +157,47 @@ class ProtectedSpeciesTests extends AbstractJdbcTemplateVueDetectionFileProcesso
         def project = loadProject(protectionLevel)
         def filter = loadFilter(speciesFilterSet)
 
+        assertCorrectResultsForListAction(authLevel, protectionLevel, speciesFilterSet, expectedResult, project, filter)
+
+        assertCorrectResultsForExportAction(authLevel, protectionLevel, speciesFilterSet, expectedResult, project, filter)
+    }
+
+    void assertCorrectResultsForListAction(AuthLevel authLevel, ProtectionLevel protectionLevel, FilterStatus speciesFilterSet, ExpectedResult expectedResult, project, filter) {
+
         controller.params.filter = filter
 
-        def actualResults = controller.list().entityList.grep {
-            DetectionSurgery.findAllByDetection(it).first()?.surgery.release.project == project
+        def resultsForProject = { detection ->
+
+            if (!detection) {
+                return false
+            }
+
+            DetectionSurgery.findAllByDetection(ValidDetection.get(detection.id)).first()?.surgery.release.project == project
         }
 
-        def description = "${authLevel} ${protectionLevel} ${speciesFilterSet} ${expectedResult} ${actualResults}"
+        def resultsFromListAction = controller.list().entityList.grep(resultsForProject)
 
-        println description
+        def description = "Checking list action:   ${authLevel} ${protectionLevel} ${speciesFilterSet} ${expectedResult} p:${project} f:${filter}"
 
         switch (expectedResult) {
             case VISIBLE:
-                assertEquals(description, 1, actualResults.size())
-                assertNotSanitised(description, actualResults[0])
+                assertEquals(description, 1, resultsFromListAction.size())
+
+                def detection = resultsFromListAction.first()
+                assertEquals description, "37010003 - Carcharodon carcharias (White Shark)", detection.speciesNames
+                assertEquals description, project.tags.sort().first().toString(), detection.sensorIds
                 break
 
             case VISIBLE_BUT_SANITISED:
-                assertEquals(description, 1, actualResults.size())
-                assertSanitised(description, actualResults[0])
+                assertEquals description, 1, resultsFromListAction.size()
+
+                def detection = resultsFromListAction.first()
+                assertEquals description, "", detection.speciesNames
+                assertEquals description, "", detection.sensorIds
                 break
 
             case NOT_VISIBLE:
-                assertTrue(description, actualResults.isEmpty())
+                assertEquals description, [], resultsFromListAction
                 break
 
             default:
@@ -121,12 +205,43 @@ class ProtectedSpeciesTests extends AbstractJdbcTemplateVueDetectionFileProcesso
         }
     }
 
-    void assertSanitised(description, detection) {
-        //fail "Not implemented"
-    }
+    void assertCorrectResultsForExportAction(AuthLevel authLevel, ProtectionLevel protectionLevel, FilterStatus speciesFilterSet, ExpectedResult expectedResult, project, filter) {
 
-    void assertNotSanitised(description, detection) {
-        //fail "Not implemented"
+        controller.params.filter = filter
+
+        controller.export()
+
+        def resultsFromExportAction = controller.response.contentAsString
+        def resultsForProject = resultsFromExportAction.readLines().findAll{
+            def tag = project.tags.sort().first()
+            it.contains(tag.toString())
+        }
+
+        def description = "Checking export action: ${authLevel} ${protectionLevel} ${speciesFilterSet} ${expectedResult} p:${project} f:${filter}"
+
+        switch (expectedResult) {
+            case VISIBLE:
+                assertEquals description, 1, resultsForProject.size()
+
+                def detectionExport = resultsForProject.first()
+                assertTrue description, detectionExport.contains(",37010003 - Carcharodon carcharias (White Shark),")
+                assertTrue description, detectionExport.contains(",${project.tags.sort().first()},")
+                break
+
+            case VISIBLE_BUT_SANITISED:
+                assertEquals(description, 1, resultsForProject.size())
+
+                def detectionExport = resultsForProject.first()
+                assertFalse description, detectionExport.contains(",37010003 - Carcharodon carcharias (White Shark),")
+                break
+
+            case NOT_VISIBLE:
+                assertEquals description, [], resultsForProject
+                break
+
+            default:
+                fail "Unhandled visibility $expectedResult"
+        }
     }
 
     void configureAccess(authLevel) {

b55efae66d02255a2a65b279a7666c292cd5bfb2
Jon Burgess <jkburges@gmail.com>
2014-11-28 11:14:13 +1100

    Formatting.

--- web-app/js/listFilter.js
+++ web-app/js/listFilter.js
@@ -1,20 +1,17 @@
-$(function()
-{
-	function submit()
-	{
-		$(".body").block({ message: blockUIConfig.defaultMessage });
-		$("#listControlForm").submit();
-	}
-	
+$(function() {
+    function submit() {
+      $(".body").block({ message: blockUIConfig.defaultMessage });
+      $("#listControlForm").submit();
+    }
+
     function split( val ) {
         return val.split( /,\s*/ );
     }
-	
-	$(".reportFilter").find(".ui-autocomplete-input").autocomplete({
+
+    $(".reportFilter").find(".ui-autocomplete-input").autocomplete({
         // Override the version in ajaxMultiSelectReportParameter.js, to also submit the form
         // (because we don't want to do it in change() below, see: http://redmine.emii.org.au/issues/937
-        select: function( event, ui ) 
-        {
+        select: function( event, ui ) {
             var terms = split( this.value );
             // remove the current input
             terms.pop();
@@ -23,21 +20,20 @@ $(function()
             // add placeholder to get the comma-and-space at the end
             terms.push( "" );
             this.value = terms.join( ", " );
-            
+
             submit();
-            
+
             return false;
         },
-	    change: function(event, ui) 
-	    {
-			submit();
-	    	
-	        return false;
-	    }
-	});
-	
-	$(".reportFilter").find(":input").not(".ui-autocomplete-input").change(function()
-	{
-		submit();
-	});
+
+        change: function(event, ui) {
+            submit();
+
+            return false;
+        }
+    });
+
+    $(".reportFilter").find(":input").not(".ui-autocomplete-input").change(function() {
+        submit();
+    });
 });

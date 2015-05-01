08062be088ba2ec36f3a1593e8d07606508a8323
Jon Burgess <jkburges@gmail.com>
2015-05-01 17:05:55 +1000

    Added views which mimic the current (in)valid_detection tables
    The main difference being that validity is derived (from the existence or not of associated metadata).
    
    Another small difference, which can possibly be addressed, is that the derived validity in this change doesn't differentiate between no associated deployment and no associated recovery.  This may or may not be worth solving.
    
    This change puts the database in a position where we can start running benchmarks and hopefully be mostly compatible with the current code base (except for the view names - this can be easily changed later).

--- grails-app/migrations/join_dets_and_rxrs.groovy
+++ grails-app/migrations/join_dets_and_rxrs.groovy
@@ -60,4 +60,79 @@ databaseChangeLog = {
 
         addForeignKeyConstraint(baseColumnNames: "receiver_download_id", baseTableName: "detection", constraintName: "receiver_download_id_detection_fk", deferrable: "false", initiallyDeferred: "false", onDelete: "NO ACTION", onUpdate: "NO ACTION", referencedColumnNames: "id", referencedTableName: "receiver_download_file", referencesUniqueColumn: "false")
     }
+
+    changeSet(author: "jburgess", id: "1430268900000-02", runOnChange: true) {
+
+        createView(
+            '''select device.id, model_name || '-' || serial_number as receiver_name
+               from device
+               join device_model on device.model_id = device_model.id
+               where device.class = 'au.org.emii.aatams.Receiver' ''',
+            viewName: 'receiver'
+        )
+
+        createView(
+            '''select receiver_id, receiver_deployment.id as receiver_deployment_id,
+                 receiver_recovery.id as receiver_recovery_id,
+                 initialisationdatetime_timestamp, recoverydatetime_timestamp
+               from receiver_recovery
+               join receiver_deployment on receiver_recovery.deployment_id = receiver_deployment.id''',
+            viewName: 'deployment_and_recovery'
+        )
+
+        createProcedure(
+            '''CREATE FUNCTION invalid_reason(
+                 detection detection,
+                   receiver receiver,
+                   deployment_and_recovery deployment_and_recovery)
+               RETURNS text AS $$
+                 BEGIN
+                   IF receiver.id IS NULL THEN
+                     RETURN 'UNKNOWN_RECEIVER';
+                   END IF;
+
+                   IF deployment_and_recovery.receiver_deployment_id IS NULL THEN
+                     RETURN 'NO_DEPLOYMENT_OR_RECOVERY_AT_DATE_TIME';
+                   END IF;
+
+                   RETURN NULL;
+                 END;
+               $$ LANGUAGE plpgsql;'''
+        )
+
+        createView(
+            '''select detection.id, timestamp, detection.receiver_name, transmitter_id,
+                 initialisationdatetime_timestamp, recoverydatetime_timestamp, receiver.id as receiver_id,
+                 deployment_and_recovery.receiver_deployment_id, deployment_and_recovery.receiver_recovery_id,
+                 invalid_reason(detection, receiver, deployment_and_recovery) as derived_reason, reason as set_reason,
+                 location, receiver_download_id, sensor_unit, sensor_value, station_name,
+                 transmitter_name, transmitter_serial_number
+
+               from detection
+               left join receiver on detection.receiver_name = receiver.receiver_name
+               left join deployment_and_recovery
+                 on receiver.id = deployment_and_recovery.receiver_id
+                 and detection.timestamp >= initialisationdatetime_timestamp
+                 and detection.timestamp <= recoverydatetime_timestamp;''',
+            viewName: 'detection_with_reason'
+        )
+
+        createView(
+            '''select detection_with_reason.id, 0 as version, location, receiver_deployment_id,
+                 receiver_download_id, receiver_name, sensor_unit, sensor_value, station_name,
+                 timestamp, transmitter_id, transmitter_name, transmitter_serial_number, false as provisional
+               from detection_with_reason
+               where derived_reason is NULL''',
+            viewName: 'valid_detection_view'
+        )
+
+        createView(
+            '''select detection_with_reason.id, 0 as version, location, 'message' as message, derived_reason as reason,
+                 receiver_download_id, receiver_name, sensor_unit, sensor_value, station_name,
+                 timestamp, transmitter_id, transmitter_name, transmitter_serial_number
+               from detection_with_reason
+               where derived_reason is not NULL''',
+            viewName: 'invalid_detection_view'
+        )
+    }
 }

28982f2fb7366d51f46f482e2c4ec904dc68ee5a
Jon Burgess <jkburges@gmail.com>
2015-06-04 12:46:36 +1000

    test fix

--- src/groovy/au/org/emii/aatams/bulk/InstallationLoader.groovy
+++ src/groovy/au/org/emii/aatams/bulk/InstallationLoader.groovy
@@ -25,14 +25,14 @@ import com.vividsolutions.jts.geom.Point;
 class InstallationLoader extends AbstractLoader
 {
     private static final Logger log = Logger.getLogger(InstallationLoader)
-    
+
     static final String GRP_ID_COL = "GRP_ID"
     static final String GRP_NAME_COL = "GRP_NAME"
     static final String STA_ID_COL = "STA_ID"
-    
+
     static final String STA_CONTACT_NAME_COL = "STA_CONTACT_NAME"
     static final String STA_SITE_NAME_COL = "STA_SITE_NAME"
-    
+
     static final String MODIFIED_DATETIME_COL = "MODIFIED_DATETIME"
     static final String MODIFIED_BY_COL = "MODIFIED_BY"
 
@@ -41,7 +41,7 @@ class InstallationLoader extends AbstractLoader
         Map groupings = loadGroupings(streams[0])
         Map groupingDetail = loadGroupingDetail(streams[1])
         Map stations = loadStations(streams[2])
-        
+
         // This will be set properly later on when the deployments are loaded.
         Point defaultLocation = new GeometryFactory().createPoint(new Coordinate(0f, 0f))
         defaultLocation.setSRID(4326)
@@ -49,12 +49,12 @@ class InstallationLoader extends AbstractLoader
         groupingDetail.each
         {
             grpId, grpDetail ->
-            
+
             log.debug("Loading grouping detail: " + grpDetail)
-            
+
             // Take contact name from first station in the group.
             def stationsIdForGrp = groupings[grpId]
-            
+
             if ((stationsIdForGrp == null) || stationsIdForGrp.isEmpty())
             {
                 // This grouping has no stations.
@@ -63,30 +63,30 @@ class InstallationLoader extends AbstractLoader
             {
                 def firstStationInGrp = stations[stationsIdForGrp[0]]
                 def contactName = firstStationInGrp[STA_CONTACT_NAME_COL]
-                
-                Installation installation = 
+
+                Installation installation =
                     createInstallation(organisation: context.bulkImport.organisation,
                                        installationName: grpDetail[GRP_NAME_COL],
                                        contactName: contactName)
-                    
+
                 stationsIdForGrp.each
                 {
                     stationId ->
-                
+
                     def stationRecord = stations[stationId]
-                    
+
                     log.debug("Loading station record: " + stationRecord)
-                    
-                    InstallationStation station = 
+
+                    InstallationStation station =
                         new InstallationStation(
                             installation: installation,
                             name: stationRecord[STA_SITE_NAME_COL],
                             curtainPosition: 0,
                             numDeployments: 0,
                             location: defaultLocation)
-                        
-                    station.save(failOnError: true)    
-                        
+
+                    station.save(failOnError: true)
+
                     BulkImportRecord importRecord =
                         new BulkImportRecord(
                             bulkImport: context.bulkImport,
@@ -96,140 +96,140 @@ class InstallationLoader extends AbstractLoader
                             dstClass: "au.org.emii.aatams.InstallationStation",
                             dstPk: station.id,
                             type: BulkImportRecordType.NEW)
-                        
+
                     importRecord.save(failOnError: true)
                 }
-            }    
+            }
         }
     }
-    
+
     private Map<Integer, Map<String, String>> loadGroupingDetail(InputStream groupingDetailStream)
     {
         def retMap = [:]
-        
+
         new CSVMapReader(new InputStreamReader(groupingDetailStream)).toList().each
         {
             record ->
-            
+
             retMap[Integer.valueOf(record[GRP_ID_COL])] = record
         }
-        
+
         return retMap
     }
-    
+
     private Map<Integer, List<Integer>> loadGroupings(InputStream groupingsStream)
     {
         Set<Integer> alreadyGroupedStations = new TreeSet<Integer>()
-        
+
         def retMap = [:]
-        
+
         new CSVMapReader(new InputStreamReader(groupingsStream)).toList().each
         {
             record ->
-            
+
             def sta_id = Integer.valueOf(record[STA_ID_COL])
-            
+
             if (alreadyGroupedStations.contains(sta_id))
             {
-                
+
             }
             else
             {
                 alreadyGroupedStations.add(sta_id)
                 def grp_id = Integer.valueOf(record[GRP_ID_COL])
-                
+
                 def stationsForGroup = retMap[grp_id]
                 if (!stationsForGroup)
                 {
                     stationsForGroup = []
                     retMap[grp_id] = stationsForGroup
                 }
-                
+
                 stationsForGroup.add(sta_id)
             }
         }
-        
+
         return retMap
     }
-    
+
     private Map<Integer, Map<String, String>> loadStations(InputStream stationsStream)
     {
         def retMap = [:]
-        
+
         new CSVMapReader(new InputStreamReader(stationsStream)).toList().each
         {
             record ->
-            
+
             retMap[Integer.valueOf(record[STA_ID_COL])] = record
         }
-        
+
         return retMap
     }
-    
+
     private Person lookupOrCreatePerson(params)
     {
         def username = params.contactName.toLowerCase().replace(' ', '')
-        
+
         Person person = Person.findByUsername(username)
         if (!person)
         {
-            person = 
+            person =
                 new Person(
                     organisation: params.organisation,
                     name: params.contactName,
                     emailAddress: username + '@unknown.com',
-                    phoneNumber: '',
+                    phoneNumber: 'blank',
                     status: EntityStatus.ACTIVE,
                     defaultTimeZone: DateTimeZone.forID("Australia/Hobart"),
                     username: username,
                     passwordHash: new Sha256Hash(username).toHex())
-                
+
             person.save(failOnError: true)
         }
-        
+
         return person
     }
-    
+
     private Project lookupOrCreateProject(params)
     {
         // create the project
         def projectName = params.contactName + "'s project"
         Project project = Project.findByName(projectName)
-        
+
         if (!project)
         {
             project = new Project(name: projectName, status: EntityStatus.ACTIVE)
             project.save()
-            
+
             // create the orgProject
             OrganisationProject orgProject = new OrganisationProject(organisation: params.organisation, project: project)
             orgProject.save()
-            
+
             // create the person
             Person person = lookupOrCreatePerson(params)
-            
+
             // create the role
             ProjectRoleType pi = ProjectRoleType.findByDisplayName(ProjectRoleType.PRINCIPAL_INVESTIGATOR)
             assert(pi)
             ProjectRole role = new ProjectRole(person: person, project: project, roleType: pi, access: ProjectAccess.READ_WRITE)
             role.save()
         }
-        
+
         return project
     }
-    
+
     private Installation createInstallation(params)
     {
         Project project = lookupOrCreateProject(params)
-        
-        Installation installation = 
+
+        Installation installation =
             new Installation(
                 project: project,
                 name: params.installationName,
                 configuration: InstallationConfiguration.findByType("ARRAY"))
-            
+
         installation.save()
-        
+
         return installation
     }
 }

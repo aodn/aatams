c459b4c09cbb6b9e15a0de43711ee081e1eb7f0e
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2016-04-08 08:46:40 +1000

    Script to fix up positive latitudes and negative longitudes in animal_release table

---
+++ scripts/sql/xavier-changes-2016_April/aatamsDB_queries_#294.sql
@@ -0,0 +1,26 @@
+﻿set search_path = aatams, public;
+
+begin;
+
+-- Change releases with positive latitudes
+UPDATE animal_release SET capture_location = ST_POINT(ST_X(capture_location), ST_Y(capture_location) * (-1))
+WHERE ST_Y(capture_location) >= 0;
+UPDATE animal_release SET release_location = ST_POINT(ST_X(release_location), ST_Y(release_location) * (-1))
+WHERE ST_Y(release_location) >= 0;
+
+---- Test results
+SELECT ST_ASTEXT(capture_location), ST_ASTEXT(release_location) FROM animal_release
+WHERE ST_Y(capture_location) >= 0 OR ST_Y(release_location) >= 0;
+
+
+-- Change releases with negative longitudes
+UPDATE animal_release SET capture_location = ST_POINT(ST_X(capture_location) * (-1), ST_Y(capture_location))
+WHERE ST_X(capture_location) <= 0;
+UPDATE animal_release SET release_location = ST_POINT(ST_X(release_location) * (-1), ST_Y(release_location))
+WHERE ST_X(release_location) <= 0;
+
+---- Test results
+SELECT ST_ASTEXT(capture_location), ST_ASTEXT(release_location) FROM animal_release
+WHERE ST_X(capture_location) <= 0 OR ST_X(release_location) <= 0;
+
+commit;

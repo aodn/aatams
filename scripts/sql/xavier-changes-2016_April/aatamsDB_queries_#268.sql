f4909756003cadf35974ccdceb8ddbc6ae2f8a2b
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2016-04-13 08:59:12 +1000

    Got rid of the DELETE bit of the code that was problematic due to a foreign key constraint

--- scripts/sql/xavier-changes-2016_April/aatamsDB_queries_#268.sql
+++ scripts/sql/xavier-changes-2016_April/aatamsDB_queries_#268.sql
@@ -2,15 +2,7 @@
 
 begin;
 
--- 1. Clean up animal_release table by deleting animal_release records with no corresponding surgery
-DELETE FROM animal_release ar
-WHERE NOT EXISTS (SELECT release_id FROM surgery s WHERE s.release_id = ar.id);
-
----- Test results
--- SELECT * FROM animal_release ar
--- WHERE NOT EXISTS (SELECT release_id FROM surgery s WHERE s.release_id = ar.id);
-
--- 2. When capture_location is null, assign value of release_location
+-- 1. When capture_location is null, assign value of release_location
 UPDATE animal_release
 SET capture_location = release_location
 WHERE capture_location IS NULL;
@@ -30,7 +22,7 @@ WHERE capture_location IS NULL;
 -- WHERE capture_location IS NULL
 -- ORDER BY project_name;
 
--- 3. When release_location is null, assign value of capture_location
+-- 2. When release_location is null, assign value of capture_location
 UPDATE animal_release
 SET release_location = capture_location
 WHERE release_location IS NULL;

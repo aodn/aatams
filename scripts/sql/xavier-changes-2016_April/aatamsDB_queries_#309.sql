d9ef331c6b6df26b59f621f285e3d7e1e7e49aa6
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2016-04-14 10:52:11 +1000

    Add script to fix #309

---
+++ scripts/sql/xavier-changes-2016_April/aatamsDB_queries_#309.sql
@@ -0,0 +1,14 @@
+﻿set search_path = aatams, public;
+
+begin;
+
+-- Fix #309 - Delete manually entered duplicate tag entry
+DELETE FROM sensor WHERE transmitter_id = 'A69-1601-65026' AND transmitter_type_id = 33;
+
+---- Test results
+-- SELECT s.transmitter_id, s.transmitter_type_id, tt.transmitter_type_name
+-- FROM sensor s
+-- JOIN transmitter_type tt ON tt.id = s.transmitter_type_id
+-- WHERE transmitter_id = 'A69-1601-65026';
+
+commit;

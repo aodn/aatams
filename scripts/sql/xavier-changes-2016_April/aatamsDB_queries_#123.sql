ddf9b178b6d11804a00e517d98963042be7ca23b
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2016-04-22 13:00:59 +1000

    Delete all animals that do not have a corresponding animal_id in the animal_release table

---
+++ scripts/sql/xavier-changes-2016_April/aatamsDB_queries_#123.sql
@@ -0,0 +1,15 @@
+﻿set search_path = aatams, public;
+
+begin;
+
+-- Fix #123
+DELETE FROM animal a
+WHERE a.id NOT IN (SELECT animal_id FROM animal_release);
+
+---- Test results
+-- SELECT a.* FROM animal a 
+-- FULL JOIN animal_release ar ON ar.animal_id = a.id 
+-- WHERE animal_id IS NULL 
+-- ORDER BY a.id;
+
+commit;

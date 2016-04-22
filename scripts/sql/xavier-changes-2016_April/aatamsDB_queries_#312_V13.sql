7424b5ceebe434dd0c837f11e7ad42ac6531e427
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2016-04-22 12:39:45 +1000

    Delete V13 transmitter duplicate

---
+++ scripts/sql/xavier-changes-2016_April/aatamsDB_queries_#312_V13.sql
@@ -0,0 +1,17 @@
+﻿set search_path = aatams, public;
+
+begin;
+
+-- Fix #312 - V13
+DELETE FROM sensor WHERE id = 128427911; 
+DELETE FROM surgery WHERE id = 128427913;
+DELETE FROM device WHERE id = 128427910;
+
+---- Test results
+-- SELECT d.*, s.*, su.*
+-- FROM surgery su
+-- JOIN device d ON su.tag_id = d.id
+-- JOIN sensor s ON s.tag_id = d.id
+-- WHERE s.transmitter_id = 'A69-1303-34797';
+
+commit;

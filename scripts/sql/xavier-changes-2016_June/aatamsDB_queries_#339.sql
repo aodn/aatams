6f2cbdf39a3c4647220c386c741a485c996864d0
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2016-06-15 08:03:32 +1000

    Query to fix #339

---
+++ scripts/sql/xavier-changes-2016_June/aatamsDB_queries_#339.sql
@@ -0,0 +1,18 @@
+﻿set search_path = aatams, public;
+
+begin;
+
+-- Fix #339 - Delete erroneous entry for receiver VR2W-102023
+DELETE FROM receiver_deployment WHERE id = 117606505;
+
+---- Test results
+--   SELECT (dm.model_name::text || '-'::text) || d.serial_number::text AS receiver_name, 
+-- 	rd.id AS receiver_deployment_id, 
+-- 	rd.deploymentdatetime_timestamp, 
+-- 	rd.initialisationdatetime_timestamp
+--   FROM device ds
+--   JOIN device_model dm ON dm.id = d.model_id
+--   JOIN receiver_deployment rd ON rd.receiver_id = d.id
+-- 	WHERE (dm.model_name::text || '-'::text) || d.serial_number::text = 'VR2W-102023';
+
+commit;
